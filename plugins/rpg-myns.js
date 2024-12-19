740a7b8f35e9c35da4a8a17b343725bc3bc2ba64
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-19 15:29:14 -0500

    Actualizar rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -3,7 +3,7 @@ import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
 m.reply(`
-▢ *${mssg.numSn}* : ${sn} ${error.message}; message);`.trim())
+▢ *${mssg.numSn}* : ${sn} Err`.trim())
 }
 handler.help = ['mysn']
 handler.tags = ['rpg']
