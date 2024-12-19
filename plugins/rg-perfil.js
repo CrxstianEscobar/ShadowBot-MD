cdde72324e9f3b1a83db2998a2dbfe24f2d068e2
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-19 17:23:28 -0500

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -14,7 +14,7 @@ try {
         who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     }
 
-    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => imagen1);
+    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => '(https://i.ibb.co/QjgtQnR/file.jpg)');
     let { description, age } = global.db.data.users[who] || {};
     let username = conn.getName(who);
 
