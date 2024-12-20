f0131ff9f70b0274fae9c6d90cb68cad070ae8d1
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-20 08:50:34 -0500

    Actualizar downloader-aptoide.js

--- plugins/downloader-aptoide.js
+++ plugins/downloader-aptoide.js
@@ -10,7 +10,7 @@ const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
     let response = `name ${data5.name}\nvx ${data5.package}\nxx ${data5.lastup}\nvv ${data5.size}`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: `${tradutor.texto3}`}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: `paraa`}, {quoted: m});
     }
     await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
   } catch {
