4eae115a3a3955eb39e5898fedf8b4e655dcb81d
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-20 08:46:45 -0500

    Actualizar downloader-aptoide.js

--- plugins/downloader-aptoide.js
+++ plugins/downloader-aptoide.js
@@ -1,6 +1,6 @@
 import { search, download } from 'aptoide-scraper'
 
-var handler = async (m, {conn, usedPrefix, command, text}) => {
+const handler = async (m, {conn, usedPrefix, command, text}) => {
 if (!text) return conn.reply(m.chat, '💥 *Ingrese el nombre de la apk para descargarlo.*', m, rcanal)
 try {
 await m.react(rwait)
@@ -31,3 +31,26 @@ handler.command = ['apk', 'modapk', 'aptoide']
 handler.register = true
 
 export default handler
+
+
+import {search, download} from 'aptoide-scraper';
+
+const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
+
+
+ if (!text) throw `${tradutor.texto1}`;
+  try {    
+    const searchA = await search(text);
+    const data5 = await download(searchA[0].id);
+    let response = `${tradutor.texto2[0]} ${data5.name}\n${tradutor.texto2[1]}* ${data5.package}\n${tradutor.texto2[2]} ${data5.lastup}\n${tradutor.texto2[3]} ${data5.size}`
+    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
+ if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
+      return await conn.sendMessage(m.chat, {text: `${tradutor.texto3}`}, {quoted: m});
+    }
+    await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
+  } catch {
+    throw `${tradutor.texto4}`;
+  }    
+};
+handler.command = /^(apk|apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
+export default handler;
