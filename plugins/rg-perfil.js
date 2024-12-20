40ffc88b54bc2d788bf2285d9c3e24cdbb33899a
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-20 00:54:59 -0500

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -5,9 +5,15 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/kgzBh.jpg')
 let { level, exp, registered, regTime, age } = global.db.data.users[m.sender]
 let username = conn.getName(who)
+
+let api = await axios.get(`https://deliriussapi-oficial.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`);
+    let userNationalityData = api.data.result;
+    let userNationality = userNationalityData ? `${userNationalityData.name} ${userNationalityData.emoji}` : 'Desconocido';
+
 let txt = `
 🚩 *PERFIL DE USUARIO*
 ☁️ *Nombre:* ${username}
+país: ${userNationality}
 💥 *Tag:* @${who.replace(/@.+/, '')}
 🌀 *Registrado:* ${registered ? '✅': '❌'}
 `.trim()
