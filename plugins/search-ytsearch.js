e1b8f86915d601a55ea16122afbb2bad252608a0
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-20 14:37:57 -0500

    Actualizar search-ytsearch.js

--- plugins/search-ytsearch.js
+++ plugins/search-ytsearch.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import yts from "yt-search";
 import axios from 'axios';
-const { generateWAMessageContent, generateWAMessageFromContent, proto } = (await import('wiskeysocketsbaileys')).default;
+const { generateWAMessageContent, generateWAMessageFromContent, proto } = (await import('@wiskeysockets/baileys')).default;
 import FormData from "form-data";
 import Jimp from "jimp";
 
