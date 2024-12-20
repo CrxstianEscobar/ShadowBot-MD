0fabe50a65d9770a9fd67c3588154b6dee73b303
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-19 22:49:27 -0500

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -4,7 +4,7 @@ let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => https://i.ibb.co/QjgtQnR/file.jpg)
 
-    conn.sendFile(m.chat, pp, 'perfil.jpg', `m.reply`, m, { mentions: [who] })
+    conn.sendFile(m.chat, pp, 'perfil.jpg', '', m, { mentions: [who] })
 
 m.reply(`*『 PERFIL DEL USUARIO 』*
 
