385768e09da2d359056a99e2b31be3e437c2243f
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-19 21:38:56 -0500

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -1,8 +1,4 @@
 import { createHash } from 'crypto'
-import PhoneNumber from 'awesome-phonenumber'
-import fetch from 'node-fetch'
-import fs from 'fs'
-import axios from 'axios'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
 
