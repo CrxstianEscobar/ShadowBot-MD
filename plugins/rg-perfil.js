74bff08448b0b81402ae760f53fb81069fd9227b
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-19 23:10:33 -0500

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -1,7 +1,7 @@
 import { createHash } from 'crypto'
 
 let handler = async function (m, { conn, text, usedPrefix }) {
-let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
+//let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
 //let pp = await conn.profilePictureUrl(who, 'image').catch(_ => miniurl)
 
     //conn.sendFile(m.chat, pp, 'perfil.jpg', 'm.reply', m, { mentions: [who] })
