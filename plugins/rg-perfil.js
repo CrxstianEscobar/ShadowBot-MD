c225578290f4ba9718f2df8973c50c0fef9f2f76
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-19 20:14:01 -0500

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -4,27 +4,35 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 import axios from 'axios'
 
-let handler = async (m, { conn, usedPrefix, command}) => {
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let bio = await conn.fetchStatus(who).catch(_ => 'undefined')
-  let biot = bio.status?.toString() || 'Sin Info'
-  let user = global.db.data.users[who]
-  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://i.ibb.co/QjgtQnR/file.jpg')
-  let username = conn.getName(who)
-  let sn = createHash('md5').update(who).digest('hex')
-  let api = await axios.get(`https://deliriussapi-oficial.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`)
-  let userNationalityData = api.data.result
-  let userNationality = userNationalityData ? `${userNationalityData.name} ${userNationalityData.emoji}` : 'Desconocido'
-  let img = await (await fetch(`${pp}`)).buffer()
-  let txt = ` –  *PERFIL - USER*\n\n`
-      txt += `◦ *Nombre* : ${name}\n`
-      txt += `◦ *Edad* : ${age} años`\n`
-      txt += `◦ *Numero* : ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n`
-      txt += `◦ *Nacionalidad* : ${userNationality}\n`
-      txt += `◦ *Link* : wa.me/${who.split`@`[0]}\n`
-  let mentionedJid = [who]
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null)
+let handler = async (m, { conn, usedPrefix, command }) => {
+  try {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let bio = await conn.fetchStatus(who).catch(_ => 'undefined')
+    let biot = bio.status?.toString() || 'Sin Info'
+    let user = global.db.data.users[who]
+    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://i.ibb.co/QjgtQnR/file.jpg')
+    let username = conn.getName(who)
+    let sn = createHash('md5').update(who).digest('hex')
+    let api = await axios.get(`https://deliriussapi-oficial.vercel.app/tools/country?text=${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}`)
+    let userNationalityData = api.data.result
+    let userNationality = userNationalityData ? `${userNationalityData.name} ${userNationalityData.emoji}` : 'Desconocido'
+    let name = user.name
+    let age = user.age
+    let img = await (await fetch(`${pp}`)).buffer()
+    let txt = ` – *PERFIL - USER*\n\n`
+    txt += `◦ *Nombre* : ${name}\n`
+    txt += `◦ *Edad* : ${age} años`\n`
+    txt += `◦ *Numero* : ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n`
+    txt += `◦ *Nacionalidad* : ${userNationality}\n`
+    txt += `◦ *Link* : wa.me/${who.split`@`[0]}\n`
+    let mentionedJid = [who]
+    await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null)
+  } catch (error) {
+    console.error(error)
+    m.reply(`Ocurrió un error al obtener el perfil del usuario: ${error.message}`)
+  }
 }
+
 handler.help = ['perfil', 'perfil *@user*']
 handler.tags = ['rg']
 handler.command = /^(perfil|profile)$/i
