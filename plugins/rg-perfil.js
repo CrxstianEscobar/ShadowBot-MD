6f1fa4df88ac040ebd49d8b1e0884301ceca2219
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-19 22:14:05 -0500

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -2,7 +2,7 @@ import { createHash } from 'crypto'
 
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
-m.reply(`*[ ℹ️ ] Numero Serial:*\n\n▢ ${sn}`.trim())
+m.reply(`🤕`.trim())
 }
 handler.help = ['perfil']
 handler.tags = ['rg']
