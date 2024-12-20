37305c4d6345de9e94239a9265c38e850badb5b4
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-20 08:55:57 -0500

    Actualizar downloader-aptoide.js

--- plugins/downloader-aptoide.js
+++ plugins/downloader-aptoide.js
@@ -1,21 +1,33 @@
-import {search, download} from 'aptoide-scraper';
+import { search, download } from 'aptoide-scraper'
 
-const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
+var handler = async (m, {conn, usedPrefix, command, text}) => {
+if (!text) return conn.reply(m.chat, '*[ ℹ️ ] Ingrese el nombre de una apk.*', m, rcanal)
+try {
+await m.react(rwait)
+conn.reply(m.chat, '*[ ☃️ ] Descargando su aplicación...*', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: dev,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+let searchA = await search(text)
+let data5 = await download(searchA[0].id)
+let txt = `*乂  APTOIDE - DESCARGAS* 乂\n\n`
+txt += `🕵🏻 *Nombre* : ${data5.name}\n`
+txt += `🗃 *Package* : ${data5.package}\n`
+txt += `🪴 *Update* : ${data5.lastup}\n`
+txt += `⚖ *Peso* :  ${data5.size}`
+await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, null, rcanal) 
+await m.react(done)  
+if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
+return await conn.reply(m.chat, '*[ ℹ️ ] El archivo es demasiado pesado*', m, rcanal )}
+await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: fkontak})
+} catch {
+return conn.reply(m.chat, '*[ ℹ️ ] Ocurrió un fallo*', m, rcanal )}}
 
+handler.tags = ['downloder']
+handler.help = ['apkmod']
+handler.command = ['apk', 'modapk', 'aptoide']
+handler.register = true
 
- if (!text) throw `x?`;
-  try {    
-    const searchA = await search(text);
-    const data5 = await download(searchA[0].id);
-    let response = `name ${data5.name}\nvx ${data5.package}\nxx ${data5.lastup}\nvv ${data5.size}`
-    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
- if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: `paraa`}, {quoted: m});
-    }
-    await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
-  } catch {
-    throw `jaja`;
-  }    
-};
-handler.command = /^(apk|apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
-export default handler;
+export default handler
