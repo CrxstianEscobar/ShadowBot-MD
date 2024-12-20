2b84fada122e51833f15c3adcf5fd757cf21caf5
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-19 23:50:28 -0500

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -2,7 +2,7 @@ import PhoneNumber from 'awesome-phonenumber'
 import fetch from 'node-fetch'
 var handler = async (m, { conn }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => imagen1)
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/kgzBh.jpg')
 let { level, exp, registered, regTime, age } = global.db.data.users[m.sender]
 let username = conn.getName(who)
 let txt = `
