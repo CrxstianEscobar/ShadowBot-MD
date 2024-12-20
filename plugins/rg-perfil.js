05f395b9de6ed93e8fb8a32bd8ee571165e96b4d
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-19 20:43:43 -0500

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -5,3 +5,12 @@ import fs from 'fs'
 import axios from 'axios'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
+
+
+
+handler.help = ['perfil', 'perfil *@user*']
+handler.tags = ['rg']
+handler.command = /^(perfil|profile)$/i
+handler.register = true
+
+export default handler
