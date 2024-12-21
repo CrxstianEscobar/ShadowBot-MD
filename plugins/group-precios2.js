5d3979fbf3dfdce01e1bcc95d12aafcd5558f94b
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-21 12:00:51 -0500

    Actualizar group-precios2.js

--- plugins/group-precios2.js
+++ plugins/group-precios2.js
@@ -1,19 +1,18 @@
-// Código Hecho Por Niño Piña wa.me/50557865603
 let handler = async (m, { conn }) => {
 // React con un emoji al mensaje
 m.react('💫');
 // Mensaje que se enviará
-const message = "*AQUI ESTAN LOS PRECIOS.*\n\n> Bot Para Grupos💫";
+const message = "Test 20";
 if (m.isGroup) {
 // URL de la imagen
-const imageUrl = 'https://i.ibb.co/TcTRpHW/file.jpg'; // Cambia esta URL por la de la imagen que deseas enviar
+const imageUrl = 'https://qu.ax/kgzBh.jpg'; // Cambia esta URL por la de la imagen que deseas enviar
 // Envía el mensaje
 // Envía la imagen
 await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { mimetype: 'image/jpeg' });
 }
 }
-handler.help = ['precios2'];
-handler.tags = ['main'];
+handler.help = ['ts'];
+handler.tags = ['group'];
 handler.group = true;
-handler.command = ['precios2', 'p2'];
+handler.command = ['ts1', 'ts'];
 export default handler;
