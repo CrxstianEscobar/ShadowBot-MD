4892c86d0d239ebbc388657e211dfa497b552eba
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-22 05:22:42 -0500

    Actualizar fun-sorteo.js

--- plugins/fun-sorteo.js
+++ plugins/fun-sorteo.js
@@ -9,7 +9,12 @@ while (b === a)
   let mensajes = [
     `*${toM(a)} ¡Felicidades! Eres el ganador del sorteo.*\n*¡Disfruta de tu premio 🥳!*`,
     `*${toM(a)} ¡Enhorabuena! Has sido seleccionado como el ganador del sorteo. ¡Bien hecho! 🎉*`,
-    `*${toM(a)} ¡La suerte te sonríe! Has sido seleccionado como el ganador del sorteo.*\n*¡Aprovecha al máximo tu premio! 🍨*`
+    `*${toM(a)} ¡La suerte te sonríe! Has sido seleccionado como el ganador del sorteo.*\n*¡Aprovecha al máximo tu premio! 🍨*`,
+    `*${toM(a)} ¡No te qlo vas a creer! Eres el ganador del sorteo. ¡Parece que la suerte te ha sonreído!* 😯`,
+    `*${toM(a)} ¡Lo lograste! Eres el ganador del sorteo. ¡No te rindas, sigue participando y quién sabe, tal vez ganes de nuevo!* 😊`,
+    `*${toM(a)} ¡Ganaste! Eres el afortunado ganador del sorteo. 🏆*`,
+    `*${toM(a)} ¡Enhorabuena! Has sido seleccionado como el ganador del sorteo. ¡No te preocupes, no te vamos a pedir que devuelvas el premio! 🎊*`
+
 ]
 
   let mensajeAleatorio = mensajes[Math.floor(Math.random() * mensajes.length)];
@@ -17,8 +22,8 @@ while (b === a)
   m.reply(mensajeAleatorio, null, { mentions: [a, b] })
 }
 
-handler.help = ['donarsala']
-handler.tags = ['ff']
-handler.command = ['donarsala', 'sala']
+handler.help = ['sorteo']
+handler.tags = ['fun']
+handler.command = ['sorteo', 'sortear']
 handler.group = true
 export default handler
