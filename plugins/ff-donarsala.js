d32b0ad12f6e3ffb9a7d458bde24104b4d9894cc
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-22 04:35:44 -0500

    Actualizar ff-donarsala.js

--- plugins/ff-donarsala.js
+++ plugins/ff-donarsala.js
@@ -1,15 +1,27 @@
+
 let toM = a => '@' + a.split('@')[0]
+
 function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`*${toM(a)},* Oh beba vaya preparando esa sala... ☃️\n> By Shadow Bot MD`, null, {
-mentions: [a, b]
-})}
+  let ps = groupMetadata.participants.map(v => (link unavailable))
+  let a = ps.getRandom()
+  let b
+  do b = ps.getRandom()
+  while (b === a)
+
+  let mensajes = [
+    `*${toM(a)},* Oh beba vaya preparando esa sala... ☃️\n> By Shadow Bot MD`,
+    `*${toM(a)},* ¡Prepárate para la batalla! 🎮\n> By Shadow Bot MD`,
+    `*${toM(a)},* ¡Es hora de jugar! 🎲\n> By Shadow Bot MD`,
+    `*${toM(a)},* ¡Vamos a ver quién es el mejor! 💪\n> By Shadow Bot MD`
+]
+
+  let mensajeAleatorio = mensajes[Math.floor(Math.random() * mensajes.length)]
+
+  m.reply(mensajeAleatorio, null, { mentions: [a, b] })
+}
+
 handler.help = ['donarsala']
 handler.tags = ['ff']
 handler.command = ['donarsala', 'sala']
-handler.group = true 
+handler.group = true
 export default handler
