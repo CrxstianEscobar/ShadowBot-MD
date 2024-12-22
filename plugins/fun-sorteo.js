7980d01e5a4a50923381208c4d840e997da0ab52
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-22 05:06:13 -0500

    Actualizar fun-sorteo.js

--- plugins/fun-sorteo.js
+++ plugins/fun-sorteo.js
@@ -11,7 +11,7 @@ while (b === a)
     `*${toM(a)} ¡Enhorabuena! Has sido seleccionado como el ganador del sorteo. ¡Bien hecho! 🎉*`,
     `*${toM(a)} ¡La suerte te sonríe! Has sido seleccionado como el ganador del sorteo.*\n*¡Aprovecha al máximo tu premio! 🍨*`,
     `*${toM(a)} ¡No te lo vas a creer! Eres el ganador del sorteo. ¡Parece que la suerte te ha sonreído!* 😯`,
-    `*${toM(a)} `,
+    `*${toM(a)} ¡Lo lograste! Eres el ganador del sorteo. ¡No te rindas, sigue participando y quién sabe, tal vez ganes de nuevo!* 😊`,
     `*${toM(a)} `,
     `*${toM(a)} `
 ]
