03322f652a7c3e796dd69f82b90aaba602906c9c
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-22 11:41:29 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -14,7 +14,7 @@ let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 const vid = ['https://telegra.ph/file/32e696946433c03588726.mp4', 'https://telegra.ph/file/5293a73eb90a920f8948b.mp4', 'https://telegra.ph/file/d5ed2537cb22b628d8ef1.mp4']
 
 let menu = `
-> 👋🏻 Hola \`${taguser}`\ ${ucapan()}
+> 👋🏻 Hola \*${taguser}* ${ucapan()}
 *˚₊·˚₊· ͟͟͞͞➳❥ _Shadow Bot - MD_*
 *☆═━┈◈ ╰ 1.4.0 V ╯ ◈┈━═☆*
 *│* 
