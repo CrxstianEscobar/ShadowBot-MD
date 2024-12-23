b6484c043cf432e25036ac76f481e46fa68624b0
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-23 15:38:09 -0500

    Actualizar info-comprarbot.js

--- plugins/info-comprarbot.js
+++ plugins/info-comprarbot.js
@@ -1,15 +1,39 @@
 const handler = async (m, {conn}) => {
   m.reply(global.ComprarBot);
 };
-handler.command = /^(comprarbot|comprar)$/i;
+handler.command = /^(preciosbot|precios|comprarbot|comprar)$/i;
 export default handler;
 
 global.ComprarBot = `
-〔 *SHADOW - BOT* 〕
+Hola @user te presento al bot mas lindo de Whatsapp.
+Mi creador: wa.me//51927238856
+Consulta con el para adquirir el Bot.
 
-*BOT PARA GRUPO* :
-> wa.me/51927238856
+·˚ ༘₊· ͟͟͞͞꒰𝐌𝐄𝐓𝐎𝐃𝐎𝐒 𝐃𝐄 𝐏𝐀𝐆𝐎 🛍
+_- Mercado pago_
+_- Yape_
+_- Pay pal_
+_- Diamantes_
 
-*BOT PERZONALIZADO* :
-> wa.me/51927238856
-`;
+·˚ ༘₊· ͟͟͞͞꒰𝐏𝐑𝐄𝐂𝐈𝐎𝐒 💸
+╰➳ `sʜᴀᴅᴏᴡ - ʙᴏᴛ`
+╭──• `ᴘᴇsᴏs ᴀʀɢ` 🇦🇷
+╎ᜊ _1 Bot - 1500_
+╎ᜊ _3 Bot - 2500_
+╎ᜊ _6 Bot - 4000_
+╰──•
+╭──• `sᴏʟᴇs ᴘᴇ` 🇵🇪
+╎ᜊ _1 Bot - 5_
+╎ᜊ _3 Bot - 8_
+╎ᜊ _6 Bot - 14_
+╰──•
+╭──• `ᴅᴏʟᴀʀᴇs` 🇺🇸
+╎ᜊ _1 Bot - 1_
+╎ᜊ _3 Bot - 3_
+╎ᜊ _6 Bot - 6_
+╰──•
+╭──• `ᴅɪᴀᴍᴀɴᴛᴇs` 💎
+╎ᜊ _1 Bot - 100_
+╎ᜊ _3 Bot - 300_
+╎ᜊ _6 Bot - 500_
+╰──•`;
