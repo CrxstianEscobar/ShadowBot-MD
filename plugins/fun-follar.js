c2b0c6985614cc6ca5fa325a2fce416e52bbea50
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-25 14:40:13 -0500

    Actualizar fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command, text }) => {
-if (!text) return m.reply(`*Ingresa el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`)
+if (!text) return m.reply(`*[ ℹ️ ] Etiqueta a un usuario para ${command.replace('how', '')}*`)
 try {
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 m.reply(`ⓘ _Acabas de follar a ${text} poniendola en 4 como una perra, la hicistes garganta profunda y le reventaste la vagina mientras te gemia, posiblemente haya quedado embarazada. 🤰🏻_
