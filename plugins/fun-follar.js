6c128aef1b846299c2976d5289cec8a76271eaa2
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-25 14:35:32 -0500

    Actualizar fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -1,13 +1,15 @@
 let handler = async (m, { conn, command, text }) => {
-if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`
+if (!text) return m.reply(`*Ingresa el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`)
+try {
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-conn.reply(m.chat, `ⓘ _Acabas de follar a ${text} poniendola en 4 como una perra, la hiciste mamar tu vrg haciendola garganta profunda, le reventaste la vagina mientras te gemia, y posiblemente haya quedado embarazada. 🥵_
+m.reply(`ⓘ _Acabas de follar a ${text} poniendola en 4 como una perra, la hicistes garganta profunda y le reventaste la vagina mientras te gemia, posiblemente haya quedado embarazada. 🤰🏻_
 
-> Que rico papi, dale más duro eso bb que rico, meteme bien duro, hazme tu puta!..
+_${text} ya te follaron 🔥_`, null, { mentions: [user] })
+} catch (err) {
+}}
+handler.help = ['follar']
+handler.tags = ['fun']
+handler.command = /^(Follar|violar)/i
 
-_${text} ya estás contenta como te dejaron la vagina y boca llena de leche? 🦊_`, null, { mentions: [user] })
-}
-
-handler.command = /^(Follar|coger|violar|kchar)/i
 handler.register = true
 export default handler
