f2e61c6e7af14ffc904b4760bcf017f9932b8a09
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-30 08:42:19 -0500

    Actualizar downloader-spotify.js

--- plugins/downloader-spotify.js
+++ plugins/downloader-spotify.js
@@ -2,27 +2,27 @@ import { igdl } from "ruhend-scraper"
 
 let handler = async (m, { args, conn }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, 'ℹ️ *Ingresa un link de Instagram*')
+    return conn.reply(m.chat, '*[ ℹ️ ] Ingresa un link de Instagram*')
   }
   try {
     await m.react('⏳️')
-    conn.reply(m.chat, `🍧 *Enviando El Video...*`)
+    conn.reply(m.chat, `*[ 🍧 ] Enviando el Video...*`)
     let res = await igdl(args[0])
     let data = res.data
     for (let media of data) {
       await new Promise(resolve => setTimeout(resolve, 2000))
-      await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🎞️ *Tu video de instagram.*')
+      await conn.sendFile(m.chat, media.url, 'instagram.mp4', '*Tu video de instagram.*')
     }
   } catch {
     await m.react('❌')
-    conn.reply(m.chat, '⚙️ Ocurrió un error.')
+    conn.reply(m.chat, '*[ ℹ️ ] Ocurrió un error.*')
   }
 }
 
-handler.command = ['instagram2', 'ig2']
-handler.tags = ['descargas']
-handler.help = ['instagram2', 'ig2']
-handler.group = true
-handler.register = true
+handler.command = ['instagram', 'ig', 'instagram2', 'ig2']
+handler.tags = ['downloader']
+handler.help = ['instagram', 'ig']
+handler.group = false
+handler.register = false
 
 export default handler
