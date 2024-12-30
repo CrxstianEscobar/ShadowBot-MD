b106261967ac4bf024d08fec9faf9c660684aff8
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-30 17:36:21 -0500

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -6,17 +6,17 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, '🚩 *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
+return conn.reply(m.chat, '*[ ℹ️ ] Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
 }
-await conn.reply(m.chat, '🚩 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m, rcanal, )
+await conn.reply(m.chat, '*[ ℹ️ ] Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m, rcanal, )
 m.react('❄️')
 
-let sessionPath = './CrowSession/'
+let sessionPath = './ShadowSession/'
 
 try {
 
 if (!existsSync(sessionPath)) {
-return await conn.reply(m.chat, '🚩 *La carpeta está vacía*', m, rcanal, )
+return await conn.reply(m.chat, '*[ ℹ️ ] La carpeta está vacía*', m, rcanal, )
 }
 let files = await fs.readdir(sessionPath)
 let filesDeleted = 0
@@ -27,16 +27,16 @@ filesDeleted++;
 }
 }
 if (filesDeleted === 0) {
-await conn.reply(m.chat, '🚩 *La carpeta esta vacía*',  m, rcanal, )
+await conn.reply(m.chat, '*[ ℹ️ ] La carpeta esta vacía*',  m, rcanal, )
 } else {
-m.react('✨')
-await conn.reply(m.chat, `🚩 *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m, rcanal, )
-conn.reply(m.chat, `🚩 *¡Hola! ¿logras verme?*`, m, rcanal, )
+m.react('🍭')
+await conn.reply(m.chat, `*[ ℹ️ ] Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m, rcanal, )
+conn.reply(m.chat, `*[ ℹ️ ] ¡Hola! ¿logras verme?*`, m, rcanal, )
 
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err);
-await conn.reply(m.chat, '🚩 *Ocurrió un fallo*',  m, rcanal, )
+await conn.reply(m.chat, '*[ ℹ️ ] Ocurrió un fallo*',  m, rcanal, )
 }
 
 }
