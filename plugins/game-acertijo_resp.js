36bc59ec9ab0d0d4241d844d42f7fe71559cedd4
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-01 12:33:57 -0500

    Actualizar game-acertijo_resp.js

--- plugins/game-acertijo_resp.js
+++ plugins/game-acertijo_resp.js
@@ -1,26 +1,55 @@
+
 import similarity from 'similarity';
+import fs from 'fs';
 
-const handler = async (m) => {
+const timeout = 60000;
+const poin = 500;
+const threshold = 0.72;
+
+const handler = async (m, {conn, usedPrefix}) => {
+  conn.tekateki = conn.tekateki ? conn.tekateki : {};
   const id = m.chat;
-  const text = m.text.toLowerCase();
 
-  // Verificar si el mensaje es una respuesta a un juego
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys) return;
+  if (m.text.toLowerCase().startsWith(usedPrefix + 'acertijo')) {
+    if (id in conn.tekateki) {
+      conn.reply(m.chat, '*[ ℹ️ ] Estas En Juego*' , conn.tekateki[id][0]);
+      throw false;
+    }
 
-  // Verificar si el juego está activo
-  if (!(id in global.tekateki)) return m.reply('No estas en juego');
-
-  // Verificar si la respuesta es correcta
-  const json = global.tekateki[id][1];
-  const response = json.response.toLowerCase().trim();
-  if (text === response) {
-    m.reply('Respuesta correcta');
-  } else if (similarity(text, response) >= 0.72) {
-    m.reply('Casi Correcto, Intenta de Nuevo');
-  } else {
-    m.reply('Incorrecto');
+    const filePath = './plugins/_acertijo.json';
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const tekateki = JSON.parse(fileContent);
+    const json = tekateki[Math.floor(Math.random() * tekateki.length)];
+    const _clue = json.response;
+    const clue = _clue.replace(/[A-Za-z]/g, '_');
+    const caption = ` ⷮ *${json.question}* Tiempo restante: ${(timeout / 1000).toFixed(2)} segundos Puntos: +${poin} Exp `.trim();
+    conn.tekateki[id] = [ await conn.reply(m.chat, caption, m), json, poin, setTimeout(async () => {
+      if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo! La respuesta era: ${json.response}`, conn.tekateki[id][0]);
+      delete conn.tekateki[id];
+    }, timeout)];
+  } else if (m.quoted && m.quoted.fromMe && m.quoted.isBaileys) {
+    if (!(id in conn.tekateki)) return m.reply('No estas en juego');
+    if ((link unavailable) == conn.tekateki[id][0].id) {
+      const json = conn.tekateki[id][1];
+      const response = json.response.toLowerCase().trim();
+      const text = m.text.toLowerCase();
+      if (text === response) {
+        global.db.data.users[m.sender].exp += conn.tekateki[id][2];
+        m.reply(`Respuesta correcta\n+${conn.tekateki[id][2]} Exp`);
+        clearTimeout(conn.tekateki[id][3]);
+        delete conn.tekateki[id];
+      } else if (similarity(text, response) >= threshold) {
+        m.reply('Casi Correcto, Intenta de Nuevo');
+      } else {
+        m.reply('Incorrecto');
+      }
+    }
   }
 };
+
+handler.help = ['acertijo'];
+handler.tags = ['game'];
+handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i;
 
 export default handler;
 
