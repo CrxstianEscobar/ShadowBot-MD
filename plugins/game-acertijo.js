79307cf9b5dcb46aac0d49d191ce59ff96cc4ae3
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-01 11:36:36 -0500

    Actualizar game-acertijo.js

--- plugins/game-acertijo.js
+++ plugins/game-acertijo.js
@@ -48,10 +48,10 @@ handler.before = async function(m) {
   this.tekateki = this.tekateki ? this.tekateki : {};
   if (!(id in this.tekateki)) return m.reply('No estas en juego');
   if (m.quoted.id == this.tekateki[id][0].id) {
-    const json = this.tekateki[id][1];
+    const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
       global.db.data.users[m.sender].exp += this.tekateki[id][2];
-      m.reply(`Respuesta correcta\n+${this.tekateki[id][2]} Exp`);
+      m.reply(`Respuesta correcta\n+ Exp`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
     } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply('Casi Correcto, Intenta de Nuevo');
