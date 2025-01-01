3a4b1b5fdc7414a602120c5b8e77b4939b7c67e0
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-01 18:01:36 -0500

    Actualizar group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -1,3 +1,39 @@
+const handler = async (m, {conn, usedPrefix, text}) => {
+
+  if (isNaN(text) && !text.match(/@/g)) {
+
+  } else if (isNaN(text)) {
+    var number = text.split`@`[1];
+  } else if (!isNaN(text)) {
+    var number = text;
+  }
+
+  if (!text && !m.quoted) return conn.reply(m.chat, `Hola\n\n*┯┷*\n*┠≽ .daradmin @tag*\n*┠≽ .darpoder 🙂\n*┷┯*`, m);
+  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, '👍🏼', m);
+
+  try {
+    if (text) {
+      var user = number + '@s.whatsapp.net';
+    } else if (m.quoted.sender) {
+      var user = m.quoted.sender;
+    } else if (m.mentionedJid) {
+      var user = number + '@s.whatsapp.net';
+    }
+  } catch (e) {
+  } finally {
+    conn.groupParticipantsUpdate(m.chat, [user], 'promote');
+    conn.reply(m.chat, '😑', m);
+  }
+};
+handler.help = ['*593xxx*', '*@usuario*', '*responder chat*'].map((v) => 'promote ' + v);
+handler.tags = ['group'];
+handler.command = /^(promote|daradmin|darpoder)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+export default handler;
+
+
 /*var handler = async (m, { conn,usedPrefix, command, text }) => {
 
 if (isNaN(text) && !text.match(/@/g)){
