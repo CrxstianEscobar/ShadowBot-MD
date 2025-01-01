fd307b380d1cd5ef74a392ccb084a4c1fa49697b
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-01 09:32:26 -0500

    Actualizar downloader-spotify.js

--- plugins/downloader-spotify.js
+++ plugins/downloader-spotify.js
@@ -1,54 +1,55 @@
-import _ from 'lodash';
-import fetch from 'node-fetch';
+import _ from "lodash"
 
 let handler = async (m, { conn, command, usedPrefix, args }) => {
-  const text = _.get(args, "length") ? args.join(" ") : _.get(m, "quoted.text") || _.get(m, "quoted.caption") || _.get(m, "quoted.description") || "";
+  const text = _.get(args, "length") ? args.join(" ") : _.get(m, "quoted.text") || _.get(m, "quoted.caption") || _.get(m, "quoted.description") || ""
+  if (typeof text !== 'string' || !text.trim()) return m.reply(`✦ Ingresa una consulta\n*Ejemplo:* .${command} Joji Ew`)
 
-  if (typeof text !== 'string' || !text.trim()) return m.reply(`✦ Ingresa una consulta\n*Ejemplo:* .${command} Joji Ew`);
+  await m.reply('✦ Espere un momento...')
+  
+let d2 = await fetch(`https://rest.cifumo.biz.id/api/downloader/spotify-search?q=${text}`)
+  let ds = await d2.json()
+const dps = await fetch(`https://rest.cifumo.biz.id/api/downloader/spotify-dl?url=${ds.data[0].url}`)
+  const dp = await dps.json()
 
-  if (!text.includes("spotify")) return m.reply(`✦ La consulta debe ser un enlace de Spotify`);
+  const { title = "No encontrado", type = "No encontrado", artis = "No encontrado", durasi = "No encontrado", download, image } = dp.data
 
-  await m.reply('✦ Espere un momento...');
+  const captvid = ` *✦Título:* ${title}
+ *✧Popularidad:* ${ds.data[0].popularity}
+ *✦Tipo:* ${type}
+ *✧Artista:* ${artis}
+ *✦link:* ${text}
+ `
 
-  try {
-    const dps = await fetch(`(link unavailable));
-    const dp = await dps.json();
+  const spthumb = (await conn.getFile(image))?.data
 
-    const { title = "No encontrado", type = "No encontrado", artis = "No encontrado", durasi = "No encontrado", download, image } = dp.data;
-
-    const captvid = ` *✦Título:* ${title} *✧Duración:* ${durasi} *✦Tipo:* ${type} *✧Artista:* ${artis} *✦link:* ${text} `;
-
-    const spthumb = (await conn.getFile(image))?.data;
-
-    const infoReply = {
-      contextInfo: {
-        externalAdReply: {
-          body: `✧ En unos momentos se entrega su audio`,
-          mediaType: 1,
-          mediaUrl: text,
-          previewType: 0,
-          renderLargerThumbnail: true,
-          sourceUrl: text,
-          thumbnail: spthumb,
-          title: `S P O T I F Y - A U D I O`
-        }
+  const infoReply = {
+    contextInfo: {
+      externalAdReply: {
+        body: `✧ En unos momentos se entrega su audio`,
+        mediaType: 1,
+        mediaUrl: text,
+        previewType: 0,
+        renderLargerThumbnail: true,
+        sourceUrl: text,
+        thumbnail: spthumb,
+        title: `S P O T I F Y - A U D I O`
       }
-    };
-
-    await conn.reply(m.chat, captvid, m, infoReply);
-
-    infoReply.contextInfo.externalAdReply.body = `Audio descargado con éxito`;
-
-    await conn.sendMessage(m.chat, { audio: { url: download }, caption: captvid, mimetype: "audio/mpeg", contextInfo: infoReply.contextInfo }, { quoted: m });
-  } catch (error) {
-    console.error(error);
-    await m.reply(`✦ Ocurrió un error al descargar el audio`);
+    }
   }
-};
 
-handler.help = ["spotifydl *<link>*"];
-handler.tags = ["downloader"];
-handler.command = /^(spotifydl)$/i;
-handler.limit = true;
+  await conn.reply(m.chat, captvid, m, infoReply)
+  infoReply.contextInfo.externalAdReply.body = `Audio descargado con éxito`
+  
+    await conn.sendMessage(m.chat, {
+      audio: { url: download },
+      caption: captvid,
+      mimetype: "audio/mpeg",
+      contextInfo: infoReply.contextInfo
+    }, { quoted: m })
+}
 
-export default handler;
+handler.help = ["spotifyplay *<consulta>*"]
+handler.tags = ["downloader"]
+handler.command = /^(spotifyplay|splay)$/i
+handler.limit = true
+export default handler
