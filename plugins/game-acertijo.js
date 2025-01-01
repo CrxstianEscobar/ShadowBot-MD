ff7c08641fb476df5f4bd299d7822525c4534adc
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-01 01:29:12 -0500

    Actualizar game-acertijo.js

--- plugins/game-acertijo.js
+++ plugins/game-acertijo.js
@@ -1,37 +1,47 @@
 import fs from 'fs';
 
-
 const timeout = 60000;
 const poin = 500;
-const handler = async (m, {conn, usedPrefix}) => {
 
+const handler = async (m, { conn, usedPrefix }) => {
   conn.tekateki = conn.tekateki ? conn.tekateki : {};
   const id = m.chat;
+
   if (id in conn.tekateki) {
     conn.reply(m.chat, 'Estas en juego', conn.tekateki[id][0]);
     throw false;
   }
-  const tekateki = 'JAA';
-  /*Para agregar más preguntas vaya a la carpeta de language en el archivo json de su 
-  idioma preferido, busque "acertijo" justo después del texto4 puede agregar sus preguntas*/
-  
+
+  const tekateki = [
+    {
+      question: '¿Cuál es el animal más rápido del mundo?',
+      response: 'León'
+    },
+    {
+      question: '¿Cuál es el planeta más grande del sistema solar?',
+      response: 'Júpiter'
+    },
+    // Agrega más preguntas aquí
+  ];
+
   const json = tekateki[Math.floor(Math.random() * tekateki.length)];
   const _clue = json.response;
   const clue = _clue.replace(/[A-Za-z]/g, '_');
-  const caption = `
-ⷮ *${json.question}*
-'hola' ${(timeout / 1000).toFixed(2)} segundos
-'xd' +${poin} Exp
-`.trim();
+  const caption = ` ⷮ *${json.question}* Tiempo restante: ${(timeout / 1000).toFixed(2)} segundos. Puntos por respuesta correcta: +${poin} Exp `.trim();
+
   conn.tekateki[id] = [
-    await conn.reply(m.chat, caption, m), json,
+    await conn.reply(m.chat, caption, m),
+    json,
     poin,
     setTimeout(async () => {
-      if (conn.tekateki[id]) await conn.reply(m.chat, `?? ${json.response}`, conn.tekateki[id][0]);
+      if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo! La respuesta era: ${json.response}`, conn.tekateki[id][0]);
       delete conn.tekateki[id];
-    }, timeout)];
+    }, timeout)
+  ];
 };
+
 handler.help = ['acertijo'];
 handler.tags = ['game'];
 handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i;
+
 export default handler;
