41c82a46a9299d9148f275abd3026fabd59e4571
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-01 09:17:41 -0500

    Actualizar downloader-instagram.js

--- plugins/downloader-instagram.js
+++ plugins/downloader-instagram.js
@@ -1,1 +1,28 @@
-//®
+import { igdl } from "ruhend-scraper"
+
+let handler = async (m, { args, conn }) => {
+  if (!args[0]) {
+    return conn.reply(m.chat, '*[ ℹ️ ] Ingresa un link de Instagram*')
+  }
+  try {
+    await m.react('⏳️')
+    conn.reply(m.chat, `*[ 🍧 ] Enviando el Video...*`)
+    let res = await igdl(args[0])
+    let data = res.data
+    for (let media of data) {
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      await conn.sendFile(m.chat, media.url, 'instagram.mp4', '*Tu video de instagram.*')
+    }
+  } catch {
+    await m.react('❌')
+    conn.reply(m.chat, '*[ ℹ️ ] Ocurrió un error.*')
+  }
+}
+
+handler.command = ['instagram', 'ig', 'instagram2', 'ig2']
+handler.tags = ['downloader']
+handler.help = ['instagram', 'ig']
+handler.group = false
+handler.register = false
+
+export default handler
