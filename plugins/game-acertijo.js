a5e9412df9a7508597d02f30b6dff1f9cec3592c
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-01 11:12:02 -0500

    Actualizar game-acertijo.js

--- plugins/game-acertijo.js
+++ plugins/game-acertijo.js
@@ -1,8 +1,10 @@
 import fs from 'fs';
-
+import similarity from 'similarity';
 
 const timeout = 60000;
 const poin = 500;
+const threshold = 0.72;
+
 const handler = async (m, {conn, usedPrefix}) => {
 
   conn.tekateki = conn.tekateki ? conn.tekateki : {};
@@ -37,18 +39,7 @@ Puntos: +${poin} Exp
       delete conn.tekateki[id];
     }, timeout)];
 };
-handler.help = ['acertijo'];
-handler.tags = ['game'];
-handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i;
-export default handler;
 
-//🍻
-
-import similarity from 'similarity';
-
-
-const threshold = 0.72;
-const handler = (m) => m;
 handler.before = async function(m) {
   
 
@@ -57,7 +48,7 @@ handler.before = async function(m) {
   this.tekateki = this.tekateki ? this.tekateki : {};
   if (!(id in this.tekateki)) return m.reply('No estas en juego');
   if (m.quoted.id == this.tekateki[id][0].id) {
-    const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
+    const json = this.tekateki[id][1];
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
       global.db.data.users[m.sender].exp += this.tekateki[id][2];
       m.reply(`Respuesta correcta\n+${this.tekateki[id][2]} Exp`);
@@ -68,5 +59,31 @@ handler.before = async function(m) {
   }
   return !0;
 };
-handler.exp = 0;
+
+
+handler.help = ['acertijo'];
+handler.tags = ['game'];
+handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i;
 export default handler;
+
+/*
+handler.before = async function(m) {
+  
+
+  const id = m.chat;
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
+  this.tekateki = this.tekateki ? this.tekateki : {};
+  if (!(id in this.tekateki)) return m.reply('No estas en juego');
+  if (m.quoted.id == this.tekateki[id][0].id) {
+    const json = this.tekateki[id][1];
+    if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
+      global.db.data.users[m.sender].exp += this.tekateki[id][2];
+      m.reply(`Respuesta correcta\n+${this.tekateki[id][2]} Exp`);
+      clearTimeout(this.tekateki[id][3]);
+      delete this.tekateki[id];
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply('Casi Correcto, Intenta de Nuevo');
+    else m.reply('Incorrecto');
+  }
+  return !0;
+};
+*/
