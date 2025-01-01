fb184eaa7267a2f71a5988c323cb20dd31fd2357
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-01 10:11:18 -0500

    Actualizar game-acertijo.js

--- plugins/game-acertijo.js
+++ plugins/game-acertijo.js
@@ -22,16 +22,18 @@ const tekateki = JSON.parse(fileContent);
   const json = tekateki[Math.floor(Math.random() * tekateki.length)];
   const _clue = json.response;
   const clue = _clue.replace(/[A-Za-z]/g, '_');
+
   const caption = `
-ⷮ *${json.question}*
-${tradutor.texto2[0]} ${(timeout / 1000).toFixed(2)} segundos
-${tradutor.texto2[1]} +${poin} Exp
+ⷮ *${json.question}* 
+Tiempo restante: ${(timeout / 1000).toFixed(2)} segundos
+Puntos: +${poin} Exp
 `.trim();
+
   conn.tekateki[id] = [
     await conn.reply(m.chat, caption, m), json,
     poin,
     setTimeout(async () => {
-      if (conn.tekateki[id]) await conn.reply(m.chat, `${tradutor.texto3} ${json.response}`, conn.tekateki[id][0]);
+      if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo! La respuesta era: ${json.response}`, conn.tekateki[id][0]);
       delete conn.tekateki[id];
     }, timeout)];
 };
