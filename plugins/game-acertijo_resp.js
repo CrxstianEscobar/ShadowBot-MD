9c17bc902db0ce1b9507c782081d491a2c574c19
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-01 11:51:20 -0500

    Actualizar game-acertijo_resp.js

--- plugins/game-acertijo_resp.js
+++ plugins/game-acertijo_resp.js
@@ -7,7 +7,7 @@ handler.before = async function(m) {
 
 
   const id = m.chat;
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
   if (!(id in this.tekateki)) return m.reply('No estas en juego');
   if (m.quoted.id == this.tekateki[id][0].id) {
