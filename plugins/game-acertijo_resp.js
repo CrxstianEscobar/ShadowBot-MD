b0c9d1316ba48f4f4222c8cff51bb67cb100a66e
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-01 12:22:13 -0500

    Actualizar game-acertijo_resp.js

--- plugins/game-acertijo_resp.js
+++ plugins/game-acertijo_resp.js
@@ -1,5 +1,32 @@
 import similarity from 'similarity';
 
+const handler = async (m) => {
+  const id = m.chat;
+  const text = m.text.toLowerCase();
+
+  // Verificar si el mensaje es una respuesta a un juego
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys) return;
+
+  // Verificar si el juego está activo
+  if (!(id in global.tekateki)) return m.reply('No estas en juego');
+
+  // Verificar si la respuesta es correcta
+  const json = global.tekateki[id][1];
+  const response = json.response.toLowerCase().trim();
+  if (text === response) {
+    m.reply('Respuesta correcta');
+  } else if (similarity(text, response) >= 0.72) {
+    m.reply('Casi Correcto, Intenta de Nuevo');
+  } else {
+    m.reply('Incorrecto');
+  }
+};
+
+export default handler;
+
+/*
+import similarity from 'similarity';
+
 
 const threshold = 0.72;
 const handler = (m) => m;
@@ -23,3 +50,4 @@ handler.before = async function(m) {
   return !0;
 };
 export default handler;
+*/
