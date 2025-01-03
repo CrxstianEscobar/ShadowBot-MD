568d8aab340ca7241ce421f171fec383c924592d
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-03 03:25:07 -0500

    Actualizar downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,30 +1,41 @@
-import("@adiwajshing/baileys"))["default"];
+/* 
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat,`🚩 Ingrese un enlace de Pinterest\n\nEjemplo:\n> *${usedPrefix + command}* https://ar.pinterest.com/pin/588142032613788991/`, m);
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
 
-await m.react('🕓');
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
+
+// *[ ❀ PINTEREST (search) ]*
+import fetch from 'node-fetch'
+
+let HS = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa el texto de lo que quieres buscar en pinterest`, m)
+  
 try {
-let { dl_url, quality, size, duration, url } = await Starlights.pinterestdl(args[0]);
+let api = await fetch(`https://bk9.fun/pinterest/search?q=${text}`)
+let json = await api.json()
+if (!json || !json.BK9 || !json.BK9.length) return conn.reply(m.chat, `✧ No se encontraron resultados para ${text}.`, m)
+let randomRes = json.BK9[Math.floor(Math.random() * json.BK9.length)]
+    
+let HS = `- *Titulo :* ${randomRes.grid_title || '-'}
+- *Creador :* ${randomRes.username || '-'}
+- *Publicado :* ${randomRes.created_at}
+- *Link :* ${randomRes.pin}`
+await conn.sendMessage(m.chat, { image: { url: randomRes.images_url }, caption: HS }, { quoted: m })
 
-let txt = '`乂  P I N T E R E S T  -  D L`\n\n'
-txt += `  ✩   *Calidad* : ${quality}\n`;
-txt += `  ✩   *Tamaño* : ${size}\n`;
-txt += `  ✩   *Duracion* : ${duration}\n`;
-txt += `  ✩   *Url* : ${url}\n\n`
-txt += `> 🚩 *${textbot}*`
+} catch (error) {
+console.error(error)
+}}
 
 
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: txt, mimetype: 'video/mp4', fileName:  `pinterest.mp4`}, {quoted: m })
-await m.react('✅');
-} catch {
-await m.react('✖️');
-}
-};
+HS.command = ['pinterest']
 
-handler.help = ['pinterestdl *<url pin>*'];
-handler.tags = ['downloader'];
-handler.command = ['pinterestdl', 'pindl'];
-handler.register = true;
-
-export default handler;
+export default HS
