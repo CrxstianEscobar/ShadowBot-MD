44c8759e557b6d67f92015707a6f22dac6f2e89a
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-03 14:43:59 -0500

    Actualizar group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `Shadow se desactivo 👍🏼`, m)
-    await m.react('🥺')
+    await conn.reply(m.chat, `*[ ℹ️ ] Se desactivó Shadow en este grupo.*`, m)
+    await m.react('😿')
 }
 handler.help = ['banearbot']
 handler.tags = ['group']
