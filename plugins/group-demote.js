c78ca0adb181a32e48e9c416148918789dffc3de
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-03 13:40:21 -0500

    Actualizar group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -1,4 +1,41 @@
-let handler = async (m, { conn,usedPrefix, command, text }) => {
+const handler = async (m, {conn, usedPrefix, text}) => {
+
+  if (isNaN(text) && !text.match(/@/g)) {
+
+  } else if (isNaN(text)) {
+    var number = text.split`@`[1];
+  } else if (!isNaN(text)) {
+    var number = text;
+  }
+
+  if (!text && !m.quoted) return conn.reply(m.chat, `🥺 ${usedPrefix}quitaradmin @tag*\n*┠≽ ${usedPrefix}quitaradmin 🍃`, m);
+  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, tradutor.texto2, m);
+
+  try {
+    if (text) {
+      var user = number + '@s.whatsapp.net';
+    } else if (m.quoted.sender) {
+      var user = m.quoted.sender;
+    } else if (m.mentionedJid) {
+      var user = number + '@s.whatsapp.net';
+    }
+  } catch (e) {
+  } finally {
+    conn.groupParticipantsUpdate(m.chat, [user], 'demote');
+    conn.reply(m.chat, `😛`, m);
+  }
+};
+handler.help = ['*593xxx*', '*@usuario*', '*responder chat*'].map((v) => 'demote ' + v);
+handler.tags = ['group'];
+handler.command = /^(demote|quitarpoder|quitaradmin)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+handler.fail = null;
+export default handler;
+// 💡💡💡💡💡💡💡💡💡💡
+
+/*let handler = async (m, { conn,usedPrefix, command, text }) => {
 if(isNaN(text) && !text.match(/@/g)){
 
 }else if(isNaN(text)) {
@@ -32,4 +69,4 @@ handler.admin = true
 handler.botAdmin = true
 handler.fail = null
 
-export default handler
+export default handler*/
