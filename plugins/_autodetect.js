92cb6f189e72487853af30b1fb2aadae9568bbe7
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-04 02:19:28 -0500

    Actualizar _autodetect.js

--- plugins/_autodetect.js
+++ plugins/_autodetect.js
@@ -1,4 +1,11 @@
-/*let WAMessageStubType = (await import('@whiskeysockets/baileys')).default
+/* ARCHIVO EDITADO , CREADO O MEJORADO
+POR EL CUERVO 
+CUERVO-TEAM-SUPREME 
+SE DEJAN CREDITOS PERRAS ES DE CUERVO 
+USO NO PRIVADO ES PUBLICO
+PERO CUERVO SE ENCARGA 
+*/
+let WAMessageStubType = (await import('@whiskeysockets/baileys')).default
 
 export async function before(m, { conn, participants, groupMetadata }) {
 if (!m.messageStubType || !m.isGroup) return
@@ -8,39 +15,39 @@ let usuario = `@${m.sender.split`@`[0]}`
 let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://qu.ax/QGAVS.jpg'  
 
 let nombre, foto, edit, newlink, status, admingp, noadmingp
-nombre = `*${usuario}*\n🌷 Ha cambiado el nombre del grupo\n\n🌻 Ahora el grupo se llama:\n${m.messageStubParameters[0]}*`
-foto = `*${usuario}*\n🌹 Ha cambiado la imagen de:\n*${groupMetadata.subject}*`
-edit = `*${usuario}*\n🌺 Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo`
-newlink = `🌸 El enlace del grupo ha sido restablecido por:\n*» ${usuario}*`
-status = `El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado 🔒*' : '*abierto 🔓*'} Por *${usuario}*\n\n💬 Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje`
-admingp = `*@${m.messageStubParameters[0].split`@`[0]}* Ahora es admin del grupo 🥳\n\n💫 Acción hecha por:\n*» ${usuario}*`
-noadmingp =  `*@${m.messageStubParameters[0].split`@`[0]}* Deja de ser admin del grupo 😿\n\n💫 Acción hecha por:\n*» ${usuario}*`
+nombre = `> ✐ *${usuario}* Ha cambiado el nombre del grupo Ahora el grupo se llama:\n*${m.messageStubParameters[0]}*`
+foto = `> ✐ *${usuario}* Ha cambiado la imagen del grupo`
+edit = `> ✐ *${usuario}* Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo`
+newlink = `> ✐ El enlace del grupo ha sido restablecido por: *» ${usuario}*`
+status = `> ✐ El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado*' : '*abierto*'} Por *${usuario}* Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje`
+admingp = `> ✐ *@${m.messageStubParameters[0].split`@`[0]}* Ahora es admin del grupo acción hecha por: *${usuario}*`
+noadmingp =  `> ✐ *@${m.messageStubParameters[0].split`@`[0]}* Deja de ser admin del grupo acción hecha por: *${usuario}*`
 
 if (chat.detect && m.messageStubType == 21) {
-await conn.sendMessage(m.chat, { text: nombre, mentions: [m.sender] }, { quoted: fkontak })   
+await conn.sendMessage(m.chat, { text: nombre, mentions: [m.sender] }, { quoted: m })   
 
 } else if (chat.detect && m.messageStubType == 22) {
-await conn.sendMessage(m.chat, { image: { url: pp }, caption: foto, mentions: [m.sender] }, { quoted: fkontak })
+await conn.sendMessage(m.chat, { image: { url: pp }, caption: foto, mentions: [m.sender] }, { quoted: m })
 
 } else if (chat.detect && m.messageStubType == 23) {
-await conn.sendMessage(m.chat, { text: newlink, mentions: [m.sender] }, { quoted: fkontak })    
+await conn.sendMessage(m.chat, { text: newlink, mentions: [m.sender] }, { quoted: m })    
 
 } else if (chat.detect && m.messageStubType == 25) {
-await conn.sendMessage(m.chat, { text: edit, mentions: [m.sender] }, { quoted: fkontak })  
+await conn.sendMessage(m.chat, { text: edit, mentions: [m.sender] }, { quoted: m })  
 
 } else if (chat.detect && m.messageStubType == 26) {
-await conn.sendMessage(m.chat, { text: status, mentions: [m.sender] }, { quoted: fkontak })  
+await conn.sendMessage(m.chat, { text: status, mentions: [m.sender] }, { quoted: m })  
 
 } else if (chat.detect && m.messageStubType == 29) {
-await conn.sendMessage(m.chat, { text: admingp, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`] }, { quoted: fkontak })  
+await conn.sendMessage(m.chat, { text: admingp, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`] }, { quoted: m })  
 
 return;
 } if (chat.detect && m.messageStubType == 30) {
-await conn.sendMessage(m.chat, { text: noadmingp, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`] }, { quoted: fkontak })  
+await conn.sendMessage(m.chat, { text: noadmingp, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`] }, { quoted: m })  
 
 } else {
 //console.log({ messageStubType: m.messageStubType,
 //messageStubParameters: m.messageStubParameters,
 //type: WAMessageStubType[m.messageStubType], 
 //})
-}}*/
+}}
