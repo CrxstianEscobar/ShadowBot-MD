fb3e1c7db9d4a2cb930b264fbdb1b1572171ce57
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-04 11:54:47 -0500

    Actualizar info-precios1.js

--- plugins/info-precios1.js
+++ plugins/info-precios1.js
@@ -32,8 +32,8 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   }
 };
 
-handler.help = ["let"].map((v) => v + " <título de la canción>");
+handler.help = ["le"].map((v) => v + " <título de la canción>");
 handler.tags = ["as"];
-handler.command = /^(let)$/i;
+handler.command = /^(le)$/i;
 
 export default handler;
