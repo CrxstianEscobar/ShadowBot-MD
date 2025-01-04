25597eb9f73d8e15d03eb0937de9318f69749c62
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-04 02:35:27 -0500

    Actualizar game-acertijo.js

--- plugins/game-acertijo.js
+++ plugins/game-acertijo.js
@@ -38,3 +38,37 @@ handler.help = ['acertijo'];
 handler.tags = ['game'];
 handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i;
 export default handler;*/
+
+
+import fs from 'fs';
+const timeout = 60000;
+const poin = 10;
+const handler = async (m, {conn, usedPrefix}) => {
+  conn.tekateki = conn.tekateki ? conn.tekateki : {};
+  const id = m.chat;
+  if (id in conn.tekateki) {
+    conn.reply(m.chat, '✐ Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0]);
+    throw false;
+  }
+  const tekateki = JSON.parse(fs.readFileSync(`./plugins/acertijo.jsos`));
+  const json = tekateki[Math.floor(Math.random() * tekateki.length)];
+  const _clue = json.response;
+  const clue = _clue.replace(/[A-Za-z]/g, '_');
+  const caption = `
+ⷮ✐ *ACERTIJOS*
+🜸 *${json.question}*
+
+✿ *Tiempo:* 60 Segundos
+✿ *Premio:* +500 money`.trim();
+  conn.tekateki[id] = [
+    await conn.reply(m.chat, caption, m), json,
+    poin,
+    setTimeout(async () => {
+      if (conn.tekateki[id]) await conn.reply(m.chat, `✐ Se acabó el tiempo!\n🜸 *Respuesta:* ${json.response}`, conn.tekateki[id][0]);
+      delete conn.tekateki[id];
+    }, timeout)];
+};
+handler.help = ['acertijo'];
+handler.tags = ['fun'];
+handler.command = ['acertijo', 'acert', 'adivinanza', 'tekateki'];
+export default handler;
