a62f2834cae06ffcade545b770023e5ece17480e
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-04 02:53:08 -0500

    Actualizar downloader-aio.js

--- plugins/downloader-aio.js
+++ plugins/downloader-aio.js
@@ -1,12 +1,13 @@
-/*import { getTracks } from "@green-code/music-track-data";
+import { getTracks } from "@green-code/music-track-data";
 import { googleImage } from "@bochilteam/scraper";
 import got from "got";
-//import cheerio from "cheerio";
+import cheerio from "cheerio";
 import fs from "fs";
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-
-if (!text && !m.quoted?.text) return conn.reply(m.chat, '*[ ℹ️ ] Error: Ingresa el título de la canción o el link del video de la canción.*', m);
+  
+  /*const teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : "";
+  if (!teks) throw `*${tradutor.texto1} ${usedPrefix + command} beret ojala*`;*/
 
 const teks = text || m.quoted?.text || '';
 if (!teks) return conn.reply(m.chat, '*[ ⚠️ ] Error: Ingresa el título de la canción o el link del video de la canción.*', m);
@@ -47,7 +48,7 @@ if (!teks) return conn.reply(m.chat, '*[ ⚠️ ] Error: Ingresa el título de l
       .replace("http://cdn-preview-", "https://cdns-preview-")
       .replace(".deezer.com", ".dzcdn.net");
 
-    const textoLetra = `ti *${tituloL || ""}*\nar  *${artistaL || ""}*\n\n$ly \n${lyrics.lyrics || "Lyrics not found."}`;
+    const textoLetra = `sotil *${tituloL || ""}*\ndoyil  *${artistaL || ""}*\n\nlytuv \n${lyrics.lyrics || "Lyrics not found."}`;
     await conn.sendMessage(
       m.chat,
       { image: { url: img }, caption: textoLetra },
@@ -64,10 +65,15 @@ if (!teks) return conn.reply(m.chat, '*[ ⚠️ ] Error: Ingresa el título de l
     );
   } catch (e) {
     console.log(`Error: ${e.message}`);
-    throw `*f vro*`;
+    throw `*Error 😿🍨*`;
   }
 };
+handler.help = ["lirik", "letra"].map((v) => v + " <Apa>");
+handler.tags = ["internet"];
+handler.command = /^(lirik|lyrics|lyric|letra)$/i;
+export default handler;
 
+/* Creditos: https://github.com/darlyn1234 */
 async function searchLyrics(term) {
   try {
     if (!term) return "🟥 Provide the name of the song to search the lyrics";
@@ -104,113 +110,3 @@ async function searchLyrics(term) {
     };
   }
 }
-
-handler.help = ["lirik", "letra"].map((v) => v + " <Apa>");
-handler.tags = ["internet"];
-handler.command = /^(lirik|lyrics|lyric|letra)$/i;
-export default handler;*/
-
-
-import { getTracks } from "@green-code/music-track-data";
-import { googleImage } from "@bochilteam/scraper";
-import got from "got";
-//import cheerio from "cheerio";
-import fs from "fs";
-
-const handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text && !m.quoted?.text) return conn.reply(m.chat, '*[ 😼 ] Error: Ingresa el título de la canción o el link del video de la canción.*', m);
-
-  try {
-    const result = await getTracks(text || m.quoted?.text);
-    let lyrics;
-    if (result) {
-      lyrics = await searchLyrics(`${result[0]?.artist} ${result[0]?.title}`);
-    } else {
-      lyrics = await searchLyrics(text || m.quoted?.text);
-    }
-
-    const tituloL = result[0].title ? result[0].title : lyrics.title;
-    const artistaL = result[0].artist ? result[0].artist : lyrics.artist;
-
-    const res = await fetch(global.API("https://some-random-api.com", "/lyrics", { title: artistaL + tituloL, }));
-    const json = await res.json();
-
-    /*let img;
-    try {
-      img = result.album.artwork;
-    } catch {
-      try {
-        img = json.thumbnail.genius;
-      } catch {
-        try {
-          const bochil = await googleImage(`${artistaL} ${tituloL}`);
-          img = await bochil.getRandom();
-        } catch {
-          img = lyrics.image;
-        }
-      }
-    }*/
-
-    const previewUrl = result[0]?.preview.replace("http://cdn-preview-", "https://cdns-preview-").replace(".deezer.com", ".dzcdn.net");
-
-   /* const textoLetra = `ti *${tituloL || ""}*\nar *${artistaL || ""}*\n\n$ly \n${lyrics.lyrics || "Lyrics not found."}`;
-
-    await conn.reply(m.chat, { image: { url: img }, caption: textoLetra }, { quoted: m });*/
-try {
-let textoLetra;
-if (lyrics.lyrics) {
-  textoLetra = `ti *${tituloL || ""}*\nar *${artistaL || ""}*\n\n$ly \n${lyrics.lyrics}`;
-} else {
-  textoLetra = "Lyrics not found.";
-}
-await conn.reply(m.chat, { text: textoLetra }, { quoted: m });
-} catch (error) {
-  console.log('Error:', error);
-  await conn.reply(m.chat, { text: 'Error al procesar la solicitud. Por favor, inténtalo de nuevo.' }, { quoted: m });
-}
-
-    await conn.reply(m.chat, { audio: { url: previewUrl }, fileName: `${artistaL || "-"} - ${tituloL || "-"}.mp3`, mimetype: "audio/mp4" }, { quoted: m });
-  } catch (e) {
-    console.log(`Error: ${e.message}`);
-    throw `*f vro*`;
-  }
-};
-
-async function searchLyrics(term) {
-  try {
-    if (!term) return " Provide the name of the song to search the lyrics";
-    const geniusResponse = await axios.get(`https://deliriussapi-oficial.vercel.app/search/genius?q=${term}`);
-    const geniusData = geniusResponse.data;
-    if (!geniusData.length) return `Couldn't find any lyrics for "${term}"`;
-    const lyricsUrl = geniusData[0].url;
-    const lyricsResponse = await axios.get(`https://deliriussapi-oficial.vercel.app/search/lyrics?url=${lyrisUrl}&parse=false`);
-    const result = {
-      status: true,
-      creador: "Sareth",
-      title: geniusData[0].title || "",
-      fullTitle: geniusData[0].fullTitle || "",
-      artist: geniusData[0].artist.name || "",
-      artistUrl: geniusData[0].artist.url || "",
-      id: geniusData[0].id || "",
-      enpoint: geniusData[0].endpoint || "",
-      instrumental: geniusData[0].instrumental,
-      image: geniusData[0].image || "",
-      url: geniusData[0].url || "",
-      lyrics: lyricsResponse.data.lyrics || "",
-    };
-    return result;
-  } catch (error) {
-    console.error(error.message);
-    return {
-      creador: "Sareth",
-      status: false,
-      message: new Error(error).message,
-    };
-  }
-}
-
-handler.help = ["lirik", "letra"].map((v) => v + " <Apa>");
-handler.tags = ["internet"];
-handler.command = /^(lirik|lyrics|lyric|letra)$/i;
-
-export default handler;
