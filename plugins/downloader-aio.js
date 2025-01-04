1e5309c1242c450b731b597a809a12e15cc56e5f
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-04 03:14:53 -0500

    Actualizar downloader-aio.js

--- plugins/downloader-aio.js
+++ plugins/downloader-aio.js
@@ -113,3 +113,78 @@ async function searchLyrics(term) {
     };
   }
 }*/
+
+import { getTracks } from "@green-code/music-track-data";
+import { googleImage } from "@bochilteam/scraper";
+import got from "got";
+import cheerio from "cheerio";
+import fs from "fs";
+import axios from 'axios';
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  try {
+    const teks = text || m.quoted?.text || '';
+    if (!teks) return conn.reply(m.chat, '*[ ⚠️ ] Error: Ingresa el título de la canción o el link del video de la canción.*', m);
+
+    const result = await getTracks(teks);
+    let lyrics;
+    if (result) {
+      lyrics = await searchLyrics(`${result[0]?.artist} ${result[0]?.title}`);
+    } else {
+      lyrics = await searchLyrics(`${teks}`);
+    }
+
+    if (!lyrics || !lyrics.lyrics) {
+      return conn.reply(m.chat, '*[ ⚠️ ] Error: No se encontraron letras para la canción.*', m);
+    }
+
+    const tituloL = lyrics.title;
+    const artistaL = lyrics.artist;
+    const previewUrl = result[0]?.preview.replace("http://cdn-preview-", "https://cdns-preview-").replace(".(link unavailable)", ".(link unavailable)");
+
+    await conn.sendMessage(m.chat, { text: `Letra de ${tituloL} de ${artistaL}\n\n${lyrics.lyrics}` }, { quoted: m });
+    await conn.sendMessage(m.chat, { audio: { url: previewUrl }, fileName: `${artistaL || "-"} - ${tituloL || "-"}.mp3`, mimetype: "audio/mp4" }, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    return conn.reply(m.chat, '*[ ⚠️ ] Error: Ocurrió un error al intentar obtener las letras de la canción.*', m);
+  }
+};
+
+handler.help = ["lirik", "letra"].map((v) => v + " <Apa>");
+handler.tags = ["internet"];
+handler.command = /^(lirik|lyrics|lyric|letra)$/i;
+
+export default handler;
+
+async function searchLyrics(term) {
+  try {
+    if (!term) return " Provide the name of the song to search the lyrics";
+    const geniusResponse = await axios.get(`(link unavailable));
+    const geniusData = geniusResponse.data;
+    if (!geniusData.length) return ` Couldn't find any lyrics for "${term}"`;
+    const lyricsUrl = geniusData[0].url;
+    const lyricsResponse = await axios.get(`(link unavailable));
+    const result = {
+      status: true,
+      creador: "Sareth",
+      title: geniusData[0].title || "",
+      fullTitle: geniusData[0].fullTitle || "",
+      artist: geniusData[0].artist.name || "",
+      artistUrl: geniusData[0].artist.url || "",
+      id: geniusData[0].id || "",
+      enpoint: geniusData[0].endpoint || "",
+      instrumental: geniusData[0].instrumental,
+      image: geniusData[0].image || "",
+      url: geniusData[0].url || "",
+      lyrics: lyricsResponse.data.lyrics || "",
+    };
+    return result;
+  } catch (error) {
+    console.error(error);
+    return {
+      creador: "Sareth",
+      status: false,
+      message: new Error(error).message,
+    };
+  }
+}
