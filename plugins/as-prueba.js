4486fa3ce2b7068264d14628669f854549851dfc
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-04 04:21:30 -0500

    Actualizar plugins

---
+++ plugins/as-prueba.js
@@ -0,0 +1,29 @@
+
+import axios from 'axios';
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  const teks = text || m.quoted?.text || '';
+  if (!teks) return conn.reply(m.chat, '*[ x ] Error: Ingresa el título de la canción.*', m);
+
+  try {
+    const response = await axios.get(`https://deliriussapi-oficial.vercel.app/search/genius?q=${teks}`);
+    const data = response.data;
+    if (!data.length) return conn.reply(m.chat, '*[ x ] Error: No se encontró la letra de la canción.*', m);
+
+    const lyricsUrl = data[0].url;
+    const lyricsResponse = await axios.get(`https://deliriussapi-oficial.vercel.app/search/lyrics?url=${lyricsUrl}&parse=false`);
+    const lyrics = lyricsResponse.data.lyrics;
+
+    const textoLetra = `*${data[0].title}*\n*${data[0].artist.name}*\n\n${lyrics}`;
+    await conn.reply(m.chat, textoLetra, m);
+  } catch (e) {
+    console.log(`Error: ${e.message}`);
+    throw `*Error 😿🍨*`;
+  }
+};
+
+handler.help = ["letra"].map((v) => v + " <título de la canción>");
+handler.tags = ["internet"];
+handler.command = /^(letra)$/i;
+
+export default handler;
