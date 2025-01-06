80e5d019862bbfc7473186ac0d7aeff91b1e1631
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-06 18:20:38 -0500

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,25 +1,5 @@
-/*let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
-	
-let kickte = `*[ ℹ️ ] Menciona al usuario que deseas eliminar.*`
-
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-let owr = m.chat.split`-`[0]
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*Usuario eliminado.*`)
-}
-
-handler.help = ['kick *@user*']
-handler.tags = ['gc']
-handler.command = ['kick', 'expulsar', 'ban'] 
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
-
-export default handler*/
-
 let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
-    let kickte = `*[ 🫵🏼 ] Menciona al usuario que deseas eliminar.*`
+    let kickte = `*[ ℹ️ ] Menciona al usuario que deseas eliminar.*`
 
     if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)})
 
@@ -31,7 +11,7 @@ let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) =
     let owner = groupMetadata.owner
 
     if (user === owner) {
-        return m.reply(`*No se puede eliminar al creador del grupo.*`)
+        return m.reply(`*[ ℹ️ ] No puedes eliminar al creador sel grupo.*`)
     }
 
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
