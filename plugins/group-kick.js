67e19ff368e65dac04c00372cb9290e2654c66a1
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-06 18:27:26 -0500

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) =
     let owner = groupMetadata.owner
 
     if (user === owner) {
-        return m.reply(`*[ ℹ️ ] No puedes eliminar al creador sel grupo.*`)
+        return m.reply(`*[ ℹ️ ] No puedes eliminar al creador del grupo.*`)
     }
 
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
