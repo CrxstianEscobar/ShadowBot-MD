1e45487ffe397700dcaa2385205becfd788f95f8
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-08 10:47:51 -0500

    Actualizar downloader-aptoide.js

--- plugins/downloader-aptoide.js
+++ plugins/downloader-aptoide.js
@@ -1,4 +1,4 @@
-import { search, download } from 'aptoide-scraper'
+/*import { search, download } from 'aptoide-scraper'
 
 var handler = async (m, {conn, usedPrefix, command, text}) => {
 if (!text) return conn.reply(m.chat, '*[ ℹ️ ] Ingrese el nombre de una apk.*', m, rcanal)
@@ -30,4 +30,32 @@ handler.help = ['apkmod']
 handler.command = ['apk', 'modapk', 'aptoide']
 handler.register = true
 
-export default handler
+export default handler*/
+
+
+import { search, download } from 'aptoide-scraper';
+
+const handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
+  if (!text) throw '[ℹ️] Ingrese el nombre de una apk.';
+  try {    
+    const searchA = await search(text);
+    const data5 = await download(searchA[0].id);
+    let response = `*乂  APTOIDE - DESCARGAS* 乂\n\n`
+    response += `🕵🏻 *Nombre* : ${data5.name}\n`
+    response += `🗃 *Package* : ${data5.package}\n`
+    response += `🪴 *Actualización* : ${data5.lastup}\n`
+    response += `⚖ *Peso* : ${data5.size}`;
+    await conn.sendMessage(m.chat, { image: { url: data5.icon }, caption: response }, { quoted: m });
+
+    if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
+      return await conn.sendMessage(m.chat, { text: '[ℹ️] El archivo es demasiado pesado.' }, { quoted: m });
+    }
+
+    await conn.sendMessage(m.chat, { document: { url: data5.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null }, { quoted: m });
+  } catch {
+    throw '[ℹ️] Ocurrió un fallo.';
+  }
+};
+
+handler.command = /^(apk|apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
+export default handler;
