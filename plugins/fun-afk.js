221a2604ba6c8c513bd8196b657f943b6e3fb87d
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-08 12:48:36 -0500

    Actualizar fun-afk.js

--- plugins/fun-afk.js
+++ plugins/fun-afk.js
@@ -2,8 +2,7 @@ const handler = async (m, {text}) => {
   const user = global.db.data.users[m.sender];
   user.afk = + new Date;
   user.afkReason = text;
-  m.reply(`*[ ℹ️ ] El usuario ${conn.getName(m.sender)} Estará inactivo (AFK), Por favor no lo etiqueten*\n\n*✧ Motivo de la inactividad:* ${text ? ': ' + text : ''}
-`);
+  m.reply(`*[ ℹ️ ] El usuario ${conn.getName(m.sender)} Estará inactivo, por favor no lo etiqueten*\n\n*✧ Motivo de la inactividad:* ${text ? ': ' + text : ''}`);
 };
 handler.help = ['afk [alasan]'];
 handler.tags = ['main'];
