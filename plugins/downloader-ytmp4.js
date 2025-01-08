8a64a0da98be15067210e0b05b4b9b8996869848
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-07 23:18:18 -0500

    Actualizar downloader-ytmp4.js

--- plugins/downloader-ytmp4.js
+++ plugins/downloader-ytmp4.js
@@ -1,41 +1,41 @@
-
 /* 
- *❀ By JTxs* 
-[ Canal Principal ] : (link unavailable) 
-[ Canal Rikka Takanashi Bot ] : (link unavailable) 
-[ Canal StarlightsTeam] : (link unavailable) 
-[ HasumiBot FreeCodes ] : (link unavailable) 
+
+*❀ By JTxs*
+
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
 */
 
 // *[ ❀ YTMP4 ]*
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-    if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
-    
-    await conn.react(m.chat, '⏳️') // reacción de espera
-    
-    try {
-        let api = await fetch(`https://axeel.my.id/api/download/video?url=$`)
-        let json = await api.json()
-        
-        let { title, views, likes, description, author } = json.metadata
-        
-        let HS = `- *Titulo :* ${title} 
-- *Descripcion :* ${description} 
-- *Visitas :* ${views} 
-- *Likes :* ${likes} 
-- *Autor :* ${author} 
-- *Tamaño :* ${json.downloads.size} `
-        
-        await conn.sendFile(m.chat, json.downloads.url, 'HasumiBotFreeCodes.mp4', HS, m)
-        
-        await conn.react(m.chat, '✔️') // reacción de finalizado
-    } catch (error) {
-        console.error(error)
-        await conn.reply(m.chat, '❌ Error al descargar el video', m)
-    }
-}
+if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
+
+try {
+let api = await fetch(`https://axeel.my.id/api/download/video?url=${text}`)
+let json = await api.json()
+let { title, views, likes, description, author } = json.metadata
+let HS = `- *Titulo :* ${title}
+- *Descripcion :* ${description}
+- *Visitas :* ${views}
+- *Likes :* ${likes}
+- *Autor :* ${author}
+- *Tamaño :* ${json.downloads.size}
+`
+await conn.sendFile(m.chat, json.downloads.url, 'HasumiBotFreeCodes.mp4', HS, m)
+} catch (error) {
+console.error(error)
+}}
 
 handler.command = /^(ytmp4)$/i
 
