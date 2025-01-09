1669107d5b66196997b6892238a8fceb84dbaddb
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-09 10:14:38 -0500

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -18,7 +18,7 @@ export default handler*/
 const handler = async (m, {conn, isROwner, text}) => {
   if (!process.send) throw 'Dont: node main.js\nDo: node index.js';
     // conn.readMessages([m.key])
-    await m.reply('*[ ⚠ ] Reiniciando el Bot...*\n\n*—◉ Espere un momento para volver a usar el Bot, puede tomar unos minutos.*');
+    await m.reply('*[ 🌷 ] Reiniciando el bot...*\n\n*» Espere un momento para volver a usar el Bot, puede tomar unos segundos.*');
     process.send('reset');
 };
 handler.help = ['restart'];
