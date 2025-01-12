f8b1a39915184b02870532fa10c21166a9b00fae
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-11 22:08:34 -0500

    Actualizar as-prueba.js

--- plugins/as-prueba.js
+++ plugins/as-prueba.js
@@ -1,12 +1,12 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw m.reply(`🌸 *Ingresa un link de pinterest*\n*🌹 Ejemplo:* ${usedPrefix}${command} https://pin.it/1q55U8K5K`);
+if (!text) throw m.reply(`*[ 🌷 ] Ingresa un link de pinterest*\n\n*[ 💡 ] Ejemplo:* ${usedPrefix}${command} https://pin.it/1q55U8K5K`);
 conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
         let ouh = await fetch(`https://api.agatz.xyz/api/pinterest?url=${text}`)
   let gyh = await ouh.json()
-        await conn.sendFile(m.chat, gyh.data.result, `pinvideobykeni.mp4`, `*🌷 Url:* ${gyh.data.url}`, m)
-        await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
+        await conn.sendFile(m.chat, gyh.data.result, `pinvideobykeni.mp4`, `*💫 Url:* ${gyh.data.url}`, m)
+        await conn.sendMessage(m.chat, { react: { text: '🧃', key: m.key }})
 }
 handler.help = ['pinvid *<link>*']
 handler.tags = ['descargas']
