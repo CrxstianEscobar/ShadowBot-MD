80211d0862287d3b344e7c62600ef69ed5ac63fd
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-13 00:50:26 -0500

    Actualizar downloader-yta.js

--- plugins/downloader-yta.js
+++ plugins/downloader-yta.js
@@ -1,1 +1,51 @@
-.
+import fetch from "node-fetch";
+import yts from "yt-search";
+
+let handler = async (m, { conn, text }) => {
+if (!text) {
+return m.reply("*[ 🌷 ] Ingresa un texto de lo que desee buscar en YouTube.*")
+}
+
+let ytres = await yts(text)
+let video = ytres.videos[0]
+  
+if (!video) {
+return m.reply("*[ ℹ️ ] Video no encontrado*")
+}
+
+let { title, thumbnail, timestamp, views, ago, url } = video
+
+let vistas = parseInt(views).toLocaleString("es-ES") + " vistas"
+
+let HS = `
+*Duración:* ${timestamp}
+*Vistas:* ${vistas}
+*Subido:* ${ago}
+*Enlace:* ${url}
+
+*[ ℹ️ ] Se está enviando su audio...*`
+
+let thumb = (await conn.getFile(thumbnail))?.data;
+
+let JT = {
+contextInfo: {
+externalAdReply: {
+title: title, body: "",
+mediaType: 1, previewType: 0,
+mediaUrl: url, sourceUrl: url,
+thumbnail: thumb, renderLargerThumbnail: true,
+}}}
+
+await conn.reply(m.chat, HS, m, JT)
+
+try {
+let api = await fetch(`https://api.vreden.web.id/api/ytplaymp3?query=${url}`);
+let json = await api.json()
+let { download } = json.result
+
+await conn.sendMessage(m.chat, { audio: { url: download.url }, caption: ``, mimetype: "audio/mpeg", }, { quoted: m })
+} catch (error) {
+console.error(error)    
+}}
+
+handler.command = /^(yta)$/i;
