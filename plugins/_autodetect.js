3e9fe63137fb6f11da9b22a5ab40911e54900a62
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-16 01:55:34 -0500

    Actualizar _autodetect.js

--- plugins/_autodetect.js
+++ plugins/_autodetect.js
@@ -8,8 +8,8 @@ let usuario = `@${m.sender.split`@`[0]}`
 let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/xr2m6u.jpg'  
 
 let nombre, foto, edit, newlink, status, admingp, noadmingp
-nombre = `*${usuario}*\n🍬 Ha cambiado el nombre del grupo.\n\n🍭 Ahora el grupo se llama:\n*<${m.messageStubParameters[0]}>*...`
-foto = `*${usuario}*\n🍬 Ha cambiado la imagen del grupo...`
+nombre = `*${usuario}*\n*Ha cambiado el nombre del grupo.*\n\n🍭 Ahora el grupo se llama:\n*<${m.messageStubParameters[0]}>*...`
+foto = `*${usuario}*\n*Ha cambiado la imagen del grupo...*`
 edit = `*${usuario}*\n🍬 Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo...`
 newlink = `🍬 El enlace del grupo ha sido restablecido por:\n*» ${usuario}*...`
 status = `🍬 El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado 🔒*' : '*abierto 🔓*'} Por *${usuario}*\n\n🍭 Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje...`
