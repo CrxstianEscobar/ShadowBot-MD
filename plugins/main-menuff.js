6e9bbc63941ada24fa79c4a04b7e4ca2e8030683
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-17 07:06:33 -0500

    Actualizar main-menuff.js

--- plugins/main-menuff.js
+++ plugins/main-menuff.js
@@ -12,7 +12,7 @@ let mentionedJid = [who]
 let perfil = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
 
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-/*const vid = ['https://telegra.ph/file/32e696946433c03588726.mp4', 'https://telegra.ph/file/5293a73eb90a920f8948b.mp4', 'https://telegra.ph/file/d5ed2537cb22b628d8ef1.mp4']*/
+const vid = ['https://telegra.ph/file/32e696946433c03588726.mp4', 'https://telegra.ph/file/5293a73eb90a920f8948b.mp4', 'https://telegra.ph/file/d5ed2537cb22b628d8ef1.mp4']
 
 let menu = `
 🌷 ¡Hᴏʟᴀ! ${taguser}
