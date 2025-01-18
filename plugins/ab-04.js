c07d6798295952ecc6d5a895f7531d6207b50edb
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-17 19:22:01 -0500

    Actualizar ab-04.js

--- plugins/ab-04.js
+++ plugins/ab-04.js
@@ -14,7 +14,7 @@ const j = Math.floor(Math.random() * (i + 1));
 }
 }
 try {
-await message.react(rwait)
+await message.react('👨🏻‍💻')
 conn.reply(message.chat, '🍭 Descargando Su Video, espere un momento...', message, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
