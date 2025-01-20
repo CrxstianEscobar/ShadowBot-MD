4708641bfac7b2510ac70b52f81c51391c0126d0
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-20 01:03:14 -0500

    Actualizar grupo-banchat.js

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -1,27 +1,15 @@
-/*let handler = async (m, { conn, isAdmin, isROwner }) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `*[ ℹ️ ] Se desactivó Shadow en este grupo.*`, m)
-    await m.react('😿')
-}
-handler.help = ['banearbot']
-handler.tags = ['group']
-handler.command = ['banearbot', 'banchat']
-handler.group = true 
-export default handler*/
-let handler = async (m, { conn, isAdmin, isROwner }) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
+let handler = async (m) => {
 
-    if (!global.db.data.chats[m.chat].isBanned) {
-        return await conn.reply(m.chat, '*[ ⚠️ ] Shadow ya estaba activo en este grupo.*', m)
-    }
+global.db.data.chats[m.chat].isBanned = true
+conn.reply(m.chat, `*[ ℹ️ ] Se desactivó Shadow en este grupo.*`, m)
 
-    global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '*[ ℹ️ ] Se activó Shadow en este grupo.*', m)
-    await m.react('😼')
 }
-handler.help = ['desbanearbot']
-handler.tags = ['group']
-handler.command = ['desbanearbot', 'unbanchat']
-handler.group = true 
+handler.help = ['banchat']
+handler.tags = ['grupo']
+handler.command = ['banchat']
+
+handler.botAdmin = true
+handler.admin = true 
+handler.group = true
+
 export default handler
