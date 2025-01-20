6dd8dbbf84996be438c088d102bc22b491b14660
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-19 22:50:55 -0500

    Actualizar game-trivia.js

--- plugins/game-trivia.js
+++ plugins/game-trivia.js
@@ -1,53 +1,45 @@
 import fs from 'fs';
 
-const triviaFile = './plugins/_trivia.json';
-const timeout = 60000;  // Tiempo límite para responder en milisegundos (60 segundos)
-const poin = 500;  // Puntos por cada respuesta correcta
+const timeout = 60000;
+const poin = 500;
+const handler = async (m, { conn, usedPrefix }) => {
 
-const handler = async (m, {conn, usedPrefix}) => {
+  conn.trivia = conn.trivia ? conn.trivia : {};
   const id = m.chat;
-  
-  conn.trivia = conn.trivia ? conn.trivia : {};  // Si no existe el objeto, lo crea
   if (id in conn.trivia) {
-    conn.reply(m.chat, '*[ ℹ️ ] Ya estás en un juego de trivia.*', conn.trivia[id][0]);
+    conn.reply(m.chat, '*[ ℹ️ ] Estás en un juego de trivia*', conn.trivia[id][0]);
     throw false;
   }
 
-  // Leer las preguntas de trivia
-  const fileContent = fs.readFileSync(triviaFile, 'utf8');
-  const triviaQuestions = JSON.parse(fileContent);
+  const filePath = './plugins/_trivia.json'; // Asegúrate de que el archivo de preguntas de trivia esté en este path
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const trivia = JSON.parse(fileContent);
 
-  // Elegir una pregunta al azar
-  const questionObj = triviaQuestions[Math.floor(Math.random() * triviaQuestions.length)];
-
+  const json = trivia[Math.floor(Math.random() * trivia.length)];
   const questionText = `
-ⷡ *Pregunta de Trivia:*
-${questionObj.question}
+ⷢ *Pregunta de Trivia:*
+${json.question}
 Opciones:
-${questionObj.options.join('\n')}
-`;
+A) ${json.options[0]}
+B) ${json.options[1]}
+C) ${json.options[2]}
+  `.trim();
 
   const caption = `
 • *Tiempo restante:* ${(timeout / 1000).toFixed(2)} segundos
 • *Puntos:* +${poin} Exp
-`.trim();
+  `.trim();
 
-  // Iniciar el juego de trivia
   conn.trivia[id] = [
-    await conn.reply(m.chat, questionText + caption, m), 
-    questionObj, 
+    await conn.reply(m.chat, questionText, m), json,
     poin,
     setTimeout(async () => {
-      if (conn.trivia[id]) {
-        await conn.reply(m.chat, `Se acabó el tiempo! La respuesta correcta era: ${questionObj.answer}) ${questionObj.options.find(opt => opt.startsWith(questionObj.answer))}`, conn.trivia[id][0]);
-        delete conn.trivia[id];  // Eliminar el juego cuando se acaba el tiempo
-      }
+      if (conn.trivia[id]) await conn.reply(m.chat, `Se acabó el tiempo! La respuesta era: ${json.answer}`, conn.trivia[id][0]);
+      delete conn.trivia[id];
     }, timeout)
   ];
 };
-
 handler.help = ['trivia'];
 handler.tags = ['game'];
-handler.command = /^(trivia|quiz)$/i;
-
+handler.command = /^(trivia|pregunta)$/i;
 export default handler;
