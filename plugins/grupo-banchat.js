f93836fefae85c648e3ee3fc7ab405e8d1b30faa
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-20 00:57:58 -0500

    Actualizar grupo-banchat.js

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, isAdmin, isROwner }) => {
+/*let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
     await conn.reply(m.chat, `*[ ℹ️ ] Se desactivó Shadow en este grupo.*`, m)
@@ -7,5 +7,21 @@ let handler = async (m, { conn, isAdmin, isROwner }) => {
 handler.help = ['banearbot']
 handler.tags = ['group']
 handler.command = ['banearbot', 'banchat']
+handler.group = true 
+export default handler*/
+let handler = async (m, { conn, isAdmin, isROwner }) => {
+    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
+
+    if (!global.db.data.chats[m.chat].isBanned) {
+        return await conn.reply(m.chat, '*[ ⚠️ ] Shadow ya estaba activo en este grupo.*', m)
+    }
+
+    global.db.data.chats[m.chat].isBanned = false
+    await conn.reply(m.chat, '*[ ℹ️ ] Se activó Shadow en este grupo.*', m)
+    await m.react('😼')
+}
+handler.help = ['desbanearbot']
+handler.tags = ['group']
+handler.command = ['desbanearbot', 'unbanchat']
 handler.group = true 
 export default handler
