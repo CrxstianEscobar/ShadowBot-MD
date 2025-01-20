d15ffcff2dc9e5db13aa5007de8e0bcc2940334f
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-19 22:58:42 -0500

    Actualizar game-trivia-resp.js

--- plugins/game-trivia-resp.js
+++ plugins/game-trivia-resp.js
@@ -1,47 +1,38 @@
-import similarity from 'similarity';
-const threshold = 0.72;
-
 const handler = (m) => m;
 
-handler.before = async function (m) {
+handler.before = async function(m) {
   const id = m.chat;
 
-  // Verificar si el mensaje es una respuesta al acertijo enviado por el bot
+  // Verificar si el mensaje es una respuesta al trivia enviado por el bot
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.text || !/^ⷢ/i.test(m.quoted.text)) return;
 
   this.trivia = this.trivia ? this.trivia : {};
 
   // Verificar si el juego está activo para este chat
   if (!(id in this.trivia)) {
-    // Enviar mensaje solo una vez si el juego ya terminó
-    if (!m._replied) {
-      m.reply('*Ese trivia ya ha terminado!*');
-      m._replied = true; // Marcar que ya se ha enviado el mensaje de fin
-    }
-    return;
+    return m.reply('*Ese trivia ya ha terminado!*');
   }
 
   // Verificar si el mensaje citado corresponde al trivia actual
   if (m.quoted.id === this.trivia[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.trivia[id][1]));
+    
+    // Comparar la respuesta con las opciones usando la letra (A, B, C)
+    const respuestaCorrecta = json.options[["A", "B", "C"].indexOf(m.text.toUpperCase())];
 
-    // Comprobar si la respuesta es correcta
-    if (m.text.toLowerCase() === json.answer.toLowerCase().trim()) {
+    // Comprobar si la respuesta seleccionada (A, B, o C) es correcta
+    if (respuestaCorrecta && m.text.toUpperCase() === json.answer[0].toUpperCase()) {
       m.reply(`*Respuesta correcta!*\n+${this.trivia[id][2]} exp`);
       clearTimeout(this.trivia[id][3]);
       delete this.trivia[id];
-    }
-    // Si la respuesta es similar (umbral de similitud)
-    else if (similarity(m.text.toLowerCase(), json.answer.toLowerCase().trim()) >= threshold) {
-      m.reply(`*Casi lo logras!*`);
-    }
+    } 
     // Si la respuesta es incorrecta
     else {
       m.reply('*Respuesta incorrecta!*');
     }
   }
 
-  return true; // Asegúrate de que esto sea verdadero para continuar la ejecución
+  return true;
 };
 
 handler.exp = 0;
