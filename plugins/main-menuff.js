6c790c14d5b392a329ecfaa3a092a48a97a88145
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-23 17:29:10 -0500

    Actualizar main-menuff.js

--- plugins/main-menuff.js
+++ plugins/main-menuff.js
@@ -1,4 +1,4 @@
-import fs from 'fs'
+/*import fs from 'fs'
 import fetch from 'node-fetch'
 import moment from 'moment-timezone'
 import { promises } from 'fs'
@@ -80,4 +80,94 @@ function clockString(ms) {
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}*/
+
+import fs from 'fs'
+import fetch from 'node-fetch'
+import moment from 'moment-timezone'
+import { promises } from 'fs'
+import { join } from 'path'
+
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, command }) => {
+    try {
+        await m.react('🎮')
+        let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+        let perfil = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
+
+        let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+        const videoUrl = 'https://telegra.ph/file/d5ed2537cb22b628d8ef1.mp4' // URL fija del video
+
+        let menu = `
+🌷 ¡Hᴏʟᴀ! ${taguser}
+${ucapan()}
+
+*˚₊·˚₊· ͟͟͞͞➳❥  Sʜʌᴅᴏ͟ᴡ Ɓᴏᴛ ᭃ*
+*╭╌┈╼◈ ╰ 1.4.0 ╯◈╾┈╌★*
+*│*
+*╰ ˚₊·˚₊· ͟͟͞͞➳❥  Hᴇʌᴠ፝֟ᴇлʟʏ Ƭᴇᴀᴍ 彡*
+
+╭─·˚₊· ͟͟͞͞꒰➳ *「 \`MENÚ FF\` 」*
+┊⪩ .v4fem
+┊⪩ .v4masc
+┊⪩ .v4mixto
+┊⪩ .v6fem
+┊⪩ .v6masc
+┊⪩ .v6mixto
+┊⪩ .feminterna4
+┊⪩ .mascinterna4
+┊⪩ .mixtointerna4
+┊⪩ .feminterna6
+┊⪩ .mascinterna6
+┊⪩ .mixtointerna6
+┊⪩ .donarsala
+┊⪩ .bermuda
+┊⪩ .kalahari
+┊⪩ .purgatorio
+┊⪩ .nexterra
+╰──────────── ·`.trim()
+
+        await conn.sendMessage(m.chat, {
+            video: { url: videoUrl }, // Video fijo
+            caption: menu,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                isForwarded: true,
+                forwardingScore: 999,
+                externalAdReply: {
+                    title: '⏤͟͞ू⃪ ፝͜⁞Sʜᴀᴅᴏᴡ✰⃔࿐\nSɪᴍᴘʟᴇ Bᴏᴛ Wʜᴀᴛsᴀᴘᴘ 💫',
+                    thumbnailUrl: perfil,
+                    mediaType: 1,
+                    renderLargerThumbnail: false,
+                },
+            },
+            gifPlayback: true,
+            gifAttribution: 0
+        }, { quoted: null })
+    } catch (e) {
+        await m.reply(`*[ ℹ️ ] Ocurrió un error al enviar el menú.*\n\n${e}`)
+    }
+}
+
+handler.help = ['menuff']
+handler.tags = ['main']
+handler.command = ['menuff', 'ff'] 
+handler.register = false
+export default handler
+
+function ucapan() {
+    const time = moment.tz('America/Lima').format('HH')
+    let res = "Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌉"
+    if (time >= 5) {
+        res = "Bᴜᴇɴᴀ Mᴀᴅʀᴜɢᴀᴅᴀ 🏙️"
+    }
+    if (time > 10) {
+        res = "Bᴜᴇɴ Dɪ́ᴀ 🏞️"
+    }
+    if (time >= 12) {
+        res = "Hᴇʀᴍᴏsᴀ Tᴀʀᴅᴇ 🌆"
+    }
+    if (time >= 19) {
+        res = "Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃"
+    }
+    return res
+}
