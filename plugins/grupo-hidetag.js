86377c7d7425a0768b911225c6ac4214d4fc6ecb
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-26 22:17:56 -0500

    Actualizar grupo-hidetag.js

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -37,3 +37,30 @@ handler.command = /^(hidetag|notificar|notify|n)$/i;
 handler.group = true;
 handler.admin = true;
 export default handler;
+
+import MessageType from '@whiskeysockets/baileys'
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
+
+let handler = async (m, { conn, text, participants }) => {
+let users = participants.map(u => conn.decodeJid(u.id))
+let q = m.quoted ? m.quoted : m
+let c = m.quoted ? m.quoted : m.msg
+const msg = conn.cMod(m.chat,
+generateWAMessageFromContent(m.chat, {
+[c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+text: c || ''
+}
+}, {
+userJid: conn.user.id
+}),
+text || q.text, conn.user.jid, { mentions: users }
+)
+await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+}
+handler.help = ['n <mensaje>']
+handler.tags = ['grupo']
+handler.command = ['hidetag', 'n', 'notify', 'notificar'] 
+handler.group = true
+handler.admin = true
+
+export default handler
