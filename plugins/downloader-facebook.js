be518f7f03c09c33c8cf1d6ba64ea7367b1990c3
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-30 16:32:02 -0500

    Actualizar downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -43,6 +43,6 @@ const handler = async (m, { text, conn, args, usedPrefix, command }) => {
 handler.help = ['fb *<link>*'];
 handler.tags = ['dl']
 handler.command = /^(fb|facebook|fbdl)$/i;
-handler.register = true
+handler.register = true;
 
 export default handler;                                                                                                                                                                                                                              
