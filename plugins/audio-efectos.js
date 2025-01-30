5731ef827646f2350b72b940681cebd858b27d71
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-29 19:06:54 -0500

    Actualizar audio-efectos.js

--- plugins/audio-efectos.js
+++ plugins/audio-efectos.js
@@ -45,7 +45,7 @@ let handler = async (m, { conn, args, __dirname, usedPrefix, command }) => {
                 })
                 await m.react('✅')
             })
-        } else return conn.reply(m.chat, `🧑‍💻 RESPONDA AL AUDIO O NOTA DE VOZ 🎵*`, m, rcanal)
+        } else return conn.reply(m.chat, `*[ 🔊 ] Responda al audio o nota de voz*`, m, rcanal)
     } catch {
         await m.react('✖️')
     }
