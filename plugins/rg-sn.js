303a3f23730ece84fe44912e13e7f6423b8b3649
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-31 12:34:47 -0500

    Actualizar rg-sn.js

--- plugins/rg-sn.js
+++ plugins/rg-sn.js
@@ -1,4 +1,4 @@
-import { createHash } from 'crypto'
+/*import { createHash } from 'crypto'
 
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
@@ -9,4 +9,33 @@ handler.tags = ['rg']
 handler.command = ['nserie', 'sn', 'mysn'] 
 handler.register = true
 
-export default handler
+export default handler*/
+
+import { createHash } from 'crypto';
+
+let handler = async function (m, { conn }) {
+    let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6);
+    
+    let messageContent = {
+        text: `*[ ℹ️ ] Número Serial:*\n\n▢ ${sn}`,
+        footer: "Presiona el botón para copiar tu número de serie",
+        templateButtons: [
+            {
+                index: 1,
+                quickReplyButton: {
+                    displayText: "📋 Copiar Número",
+                    id: `.copiar ${sn}`
+                }
+            }
+        ]
+    };
+
+    await conn.sendMessage(m.chat, messageContent, { quoted: m });
+};
+
+handler.help = ['mysn'];
+handler.tags = ['rg'];
+handler.command = ['nserie', 'sn', 'mysn'];
+handler.register = true;
+
+export default handler;
