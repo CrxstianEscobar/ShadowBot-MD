a31c0e4cdd56d64be74cc7f072f31b069c0c6640
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-31 12:46:22 -0500

    Actualizar rg-sn.js

--- plugins/rg-sn.js
+++ plugins/rg-sn.js
@@ -2,7 +2,7 @@ import { createHash } from 'crypto'
 
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
-m.reply(`*[ ☕ ] Numero Serial:*\n\n▢ ${sn}`.trim())
+m.reply(`*[ ☕ ] Numero Serial:*\n\n${sn}`.trim())
 }
 handler.help = ['mysn']
 handler.tags = ['rg']
