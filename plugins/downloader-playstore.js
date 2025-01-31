6fafa12d59a6fa6742a53052d2c25d2a33b8f6aa
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-31 07:46:12 -0500

    Actualizar downloader-playstore.js

--- plugins/downloader-playstore.js
+++ plugins/downloader-playstore.js
@@ -1,12 +1,11 @@
 import gplay from 'google-play-scraper';
 
 let handler = async (m, { conn, args, usedPrefix: prefix, command }) => {
-    // Verificar si hay un enlace
+
     if (!args[0]) {
-        return conn.reply(m.chat, `*☕ Ingresa el enlace de la aplicación que deseas descargar de la Play Store.*\n\n*Ejemplo:*\n\`${prefix + command} https://play.google.com/store/apps/details?id=com.whatsapp\``, m, rcanal);
+        return conn.reply(m.chat, `*☕ Ingresa el enlace de la aplicación que deseas descargar de la Play Store.*\n\n*Ejemplo:*\n\${prefix + command} https://play.google.com/store/apps/details?id=com.whatsapp`, m, rcanal);
     }
 
-    // Reacción de espera solo si se pasa un enlace
     m.react('⌛');
 
     const url = args[0];
@@ -15,31 +14,28 @@ let handler = async (m, { conn, args, usedPrefix: prefix, command }) => {
         packageName = new URL(url).searchParams.get("id");
         if (!packageName) throw new Error();
     } catch {
-        return conn.reply(m.chat, `*❌ La URL proporcionada no es válida o no contiene un ID de aplicación.*`, m, rcanal);
+        return conn.reply(m.chat, `*[ ❌ ] La URL proporcionada no es válida o no contiene un ID de aplicación.*`, m, rcanal);
     }
 
     let info;
     try {
         info = await gplay.app({ appId: packageName });
     } catch {
-        return conn.reply(m.chat, `*❌ No se pudo encontrar la aplicación. Asegúrate de que el enlace sea correcto.*`, m, rcanal);
+        return conn.reply(m.chat, `*[ ❌ ] No se pudo encontrar la aplicación. Asegúrate de que el enlace sea correcto.*`, m, rcanal);
     }
 
     const h = info.title;
     let link = `https://d.apkpure.com/b/APK/${info.appId}?version=latest`;
 
-    // Verificación de la URL de APK
     const response = await fetch(link);
     if (!response.ok) {
-        return conn.reply(m.chat, `*❌ No se pudo obtener el archivo APK. Intenta con otro enlace.*`, m, rcanal);
+        return conn.reply(m.chat, `*[ ❌ ] No se pudo obtener el archivo APK. Intenta con otro enlace.*`, m, rcanal);
     }
 
-    // Enviar el archivo APK y esperar que se termine
     await conn.sendFile(m.chat, link, `${h}.apk`, ``, m, false, { mimetype: 'application/vnd.android.package-archive', asDocument: true });
 
-    // Reacción de éxito
     m.react('✅️');
-    conn.reply(m.chat, `*¡Descarga completada para "${h}"!*`, m, rcanal);
+    conn.reply(m.chat, `*[ ✅ ] Descarga completada para "${h}*`, m, rcanal);
 }
 
 handler.help = ['playstore *<url>*']; 
