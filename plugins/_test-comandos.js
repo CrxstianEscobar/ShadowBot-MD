03193ccd9f82c4b3b5bf97702323018fe3e9adc6
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-02 22:46:55 -0500

    Actualizar _test-comandos.js

--- plugins/_test-comandos.js
+++ plugins/_test-comandos.js
@@ -1,40 +1,37 @@
-/* ౨ৎ ˖ ࣪⊹ 𝐁𝐲 𝐉𝐭𝐱𝐬 𐙚˚.ᡣ𐭩
+/* 
 
-❀ Canal Principal ≽^•˕• ྀི≼
+❀ By JTxs
+
+[ Canal Principal ] :
 https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
 
-❀ Canal Rikka Takanashi Bot
+[ Canal Rikka Takanashi Bot ] :
 https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
 
-❀ Canal StarlightsTeam
+[ Canal StarlightsTeam] :
 https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
 
-❀ HasumiBot FreeCodes 
+[ HasumiBot FreeCodes ] :
 https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
 */
 
-// *𓍯𓂃𓏧♡  PINTEREST - SEARCH*
-import axios from 'axios'
+// *[ ❀ POR N HUB DL ]*
+import fetch from 'node-fetch'
 
-let HS = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un texto para buscar en pinterest`, m)
-  
+let HS = async (m, { conn, command, text, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, '❀ ingresa un link de pornhub', m)
+//si borras creditos eri gei 👀
 try {
-let api = await axios.get(`https://api.siputzx.my.id/api/s/pinterest?query=${text}`)
-let json = api.data
-let data = json.data[Math.floor(Math.random() * json.data.length)]
-
-let { pin, created_at, images_url, grid_title } = data
-let HS = `- *Titulo :* ${grid_title}
-- *Creado :* ${created_at}
-- *link :* ${pin}`
-await conn.sendMessage(m.chat, { image: { url: images_url }, caption: HS }, { quoted: m })
-//await conn.sendMessage(m.chat, { image: images_url, caption: HS, footer: '', buttons: [ { buttonId: `.pinterest ${text}`, buttonText: { displayText: 'Siguiente' } }, ], viewOnce: true, headerType: 4 }, { quoted: m })
-    
+let api = await fetch(`https://www.dark-yasiya-api.site/download/phub?url=${text}`)
+let json = await api.json()
+let { video_title, video_uploader } = json.result
+let { download_url, resolution, } = json.result.format[1]
+await conn.sendMessage(m.chat, { video: { url: download_url }, caption: video_title }, { quoted: m })
 } catch (error) {
 console.error(error)
 }}
 
-HS.command = ['pinterest4', 'pinterestsearch4']
+HS.command = ['pornhubdl4']
 
 export default HS
+//Dejen creditos 👀 [ By Jtxs ] https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
