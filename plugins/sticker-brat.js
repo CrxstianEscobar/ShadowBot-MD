85738a57561af644cb668f5bbfde8d0ae9302bf0
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-02 22:58:06 -0500

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -1,4 +1,4 @@
-/*import axios from 'axios';
+import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
 import sharp from 'sharp';
@@ -32,7 +32,7 @@ const handler = async (m, {
 }) => {
     if (!text) {
         return conn.sendMessage(m.chat, {
-            text: '🍬 Por favor ingresa el texto para hacer un sticker.',
+            text: '*[ ☕ ] Por favor ingresa el texto para hacer un sticker.',
         }, {
             quoted: m
         });
@@ -75,67 +75,5 @@ const handler = async (m, {
 handler.command = ['brat'];
 handler.tags = ['sticker'];
 handler.help = ['brat *<texto>*'];
-*/
-import axios from 'axios';
-import fs from 'fs';
-import path from 'path';
-import { sticker } from '../lib/sticker.js'
-import uploadFile from '../lib/uploadFile.js'
-import { addExif } from '../lib/sticker.js'
-import { Sticker } from 'wa-sticker-formatter';
-
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-const fetchSticker = async (text, attempt = 1) => {
-  try {
-    const response = await axios.get(`https://kepolu-brat.hf.space/brat`, {
-      params: {
-        q: text
-      },
-      responseType: 'arraybuffer',
-    });
-    return response.data;
-  } catch (error) {
-    if (error.response?.status === 429 && attempt <= 3) {
-      const retryAfter = error.response.headers['retry-after'] || 5;
-      await delay(retryAfter * 1000);
-      return fetchSticker(text, attempt + 1);
-    }
-    throw error;
-  }
-};
-
-const handler = async (m, { text, conn }) => {
-  if (!text) {
-    return conn.sendMessage(m.chat, {
-      text: ' Por favor ingresa el texto para hacer un sticker.',
-    }, { quoted: m });
-  }
-  try {
-    const buffer = await fetchSticker(text);
-    const stickerBuffer = await sticker(buffer, {
-      pack: 'Tu paquete de stickers',
-      author: 'Tu nombre',
-    });
-    const stickerExif = await addExif(stickerBuffer, {
-      pack: 'Tu paquete de stickers',
-      author: 'Tu nombre',
-    });
-    const stickerUrl = await uploadFile(stickerExif);
-    await conn.sendMessage(m.chat, {
-      sticker: {
-        url: stickerUrl
-      },
-    }, { quoted: m });
-  } catch (error) {
-    return conn.sendMessage(m.chat, {
-      text: ' Ocurrio un error.',
-    }, { quoted: m });
-  }
-};
-
-handler.command = ['brat'];
-handler.tags = ['sticker'];
-handler.help = ['brat <texto>'];
 
 export default handler;
