ecd6c343855f9c45b7f611dd153d07766dea6acf
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-02 23:00:31 -0500

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -32,7 +32,7 @@ const handler = async (m, {
 }) => {
     if (!text) {
         return conn.sendMessage(m.chat, {
-            text: '*[ ☕ ] Por favor ingresa el texto para hacer un sticker.',rcanal)
+            text: '*[ ☕ ] Por favor ingresa el texto para hacer un sticker.'
         }, {
             quoted: m
         });
