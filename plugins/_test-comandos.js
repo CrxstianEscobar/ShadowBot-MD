3ea2bb6c33f2e00d1d1d4f0176bdaf20281477ef
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-02 21:49:50 -0500

    Actualizar _test-comandos.js

--- plugins/_test-comandos.js
+++ plugins/_test-comandos.js
@@ -21,7 +21,7 @@ let xd = 'https://files.catbox.moe/trd8vu.jpg';
 let html = `*🎩 HTML EXTRAIDO DE LA WEB:* ${data}*`
 
 m.react('✅');
-conn.sendMessage(m.chat, { image: { url: xd }, caption: html }, { quoted: fkontak });
+conn.sendMessage(m.chat, { image: { url: xd }, caption: html });
 };
 
 handler.command = ['htmlweb', 'hweb'];
