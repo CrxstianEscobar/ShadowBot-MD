2308d753b6b82db3c3572c4451f84842c09b4213
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-04 19:05:38 -0500

    Actualizar _antiArab.js

--- plugins/_antiArab.js
+++ plugins/_antiArab.js
@@ -1,70 +1,22 @@
-import fetch from "node-fetch";
-import crypto from "crypto";
-import { FormData, Blob } from "formdata-node";
-import { fileTypeFromBuffer } from "file-type";
+let handler = m => m
 
-const handler = async (m, { conn }) => {
-let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || "";
-  if (!mime) return m.reply("No media found", null, { quoted: fkontak });
-  let media = await q.download();
-let link = await catbox(media);
-  let caption = `📮 *L I N K :*
- \`\`\`• ${link}\`\`\`
-📊 *S I Z E :* ${formatBytes(media.length)}
-📛 *E x p i r e d :* "No Expiry Date" 
-`;
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner} ) {
+  if (!m.isGroup) return !1
+  let chat = global.db.data.chats[m.chat]
 
-  await m.reply(caption);
-}
-handler.command = handler.help = ['tourl2']
-handler.tags = ['tools']
-handler.diamond = true
-handler.estrellas = 5;
-export default handler
+  if (isBotAdmin && chat.onlyLatinos && !isAdmin && !isOwner) {
+    let forbidPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "234", "254", "213"]
 
-
-function formatBytes(bytes) {
-  if (bytes === 0) {
-    return "0 B";
+    for (let prefix of forbidPrefixes) {
+      if (m.sender.startsWith(prefix)) {
+        m.reply('In the Group Only Spanish Users.', m.sender)
+        await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+        return false
+      }
+    }
   }
-  const sizes = ["B", "KB", "MB", "GB", "TB"];
-  const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
+
+  return true
 }
 
-
-/**
- * Upload image to catbox
- * Supported mimetype:
- * - `image/jpeg`
- * - `image/jpg`
- * - `image/png`s
- * - `image/webp`
- * - `video/mp4`
- * - `video/gif`
- * - `audio/mpeg`
- * - `audio/opus`
- * - `audio/mpa`
- * @param {Buffer} buffer Image Buffer
- * @return {Promise<string>}
- */
-async function catbox(content) {
-  const { ext, mime } = (await fileTypeFromBuffer(content)) || {};
-  const blob = new Blob([content.toArrayBuffer()], { type: mime });
-  const formData = new FormData();
-  const randomBytes = crypto.randomBytes(5).toString("hex");
-  formData.append("reqtype", "fileupload");
-  formData.append("fileToUpload", blob, randomBytes + "." + ext);
-
-  const response = await fetch("https://catbox.moe/user/api.php", {
-    method: "POST",
-    body: formData,
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
-    },
-  });
-
-  return await response.text();
-}
+export default handler
