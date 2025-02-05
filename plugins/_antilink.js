2235775fcccd53acac6373ff62d19a738fca1caa
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-04 19:06:35 -0500

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,22 +1,33 @@
-let handler = m => m
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+let linkRegex1 = /whatsapp.com\/channel\/([0-9A-Za-z]{20,24})/i;
 
-handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner} ) {
-  if (!m.isGroup) return !1
-  let chat = global.db.data.chats[m.chat]
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, participants }) {
+if (!m.isGroup) return 
+if (isAdmin || isOwner || m.fromMe || isROwner) return
 
-  if (isBotAdmin && chat.onlyLatinos && !isAdmin && !isOwner) {
-    let forbidPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "234", "254", "213"]
-
-    for (let prefix of forbidPrefixes) {
-      if (m.sender.startsWith(prefix)) {
-        m.reply('In the Group Only Spanish Users.', m.sender)
-        await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        return false
-      }
-    }
-  }
-
-  return true
+let chat = global.db.data.chats[m.chat];
+let delet = m.key.participant;
+let bang = m.key.id;
+const user = `@${m.sender.split`@`[0]}`;
+const groupAdmins = participants.filter(p => p.admin);
+const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n');
+let bot = global.db.data.settings[this.user.jid] || {};
+const isGroupLink = linkRegex.exec(m.text) || linkRegex1.exec(m.text);
+const grupo = `https://chat.whatsapp.com`;
+if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('🍬 El antilink está activo pero te salvaste por ser admin.');
+if (chat.antiLink && isGroupLink && !isAdmin) {
+if (isBotAdmin) {
+const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
+if (m.text.includes(linkThisGroup)) return !0;
 }
-
-export default handler
+await conn.sendMessage(m.chat, { text: `*「 ENLACE DETECTADO 」*\n\n🍭 ${user} Rompiste las reglas del Grupo serás eliminado...`, mentions: [m.sender] }, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100 });
+if (!isBotAdmin) return conn.sendMessage(m.chat, { text: `🍬 El antilink está activo pero no puedo eliminarte porque no soy admin.`, mentions: [...groupAdmins.map(v => v.id)] }, { quoted: m });
+if (isBotAdmin) {
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+if (responseb[0].status === "404") return;
+}} // else if (!bot.restrict) {
+// return m.reply('🍭 ¡Esta característica está deshabilitada!');
+// }
+return !0;
+}
