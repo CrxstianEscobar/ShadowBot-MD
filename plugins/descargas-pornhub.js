3d04ab8875b25158d5a15559a683c256ce87f3c8
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-05 02:13:30 -0500

    Actualizar descargas-pornhub.js

--- plugins/descargas-pornhub.js
+++ plugins/descargas-pornhub.js
@@ -32,8 +32,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null)
     } else if (/image/g.test(mime)) {
       // Jalankan kode untuk gambar di sini
-      /*let [packname, ...author] = args.join` `.split`|`
-      author = (author || []).join`|`*/
       let packName = args[0] || 'Shadow'
       let authorName = args[1] || 'by ${userName}'
       let img = await q.download?.()
