b0247d2da77ec1b0fecda893302fa5359b0a832c
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-05 02:12:42 -0500

    Actualizar descargas-pornhub.js

--- plugins/descargas-pornhub.js
+++ plugins/descargas-pornhub.js
@@ -1,4 +1,4 @@
-import { sticker } from '../lib/sticker.js'
+/*import { sticker } from '../lib/sticker.js'
 import uploadFile from '../lib/uploadFile.js'
 import { addExif } from '../lib/sticker.js'
 import { Sticker } from 'wa-sticker-formatter'
@@ -75,4 +75,59 @@ async function createSticker(img, url, packName, authorName, quality) {
     quality
   }
   return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()
+}*/
+
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+
+    let userName = await conn.getName(m.sender) || 'Usuario' // Obtiene el nombre del usuario
+
+    if (/video/g.test(mime)) {
+      if ((q.msg || q).seconds > 10) return m.reply('*[ ℹ️ ] Máximo 10 segundos.*')
+      let img = await q.download?.()
+      if (!img) throw m.reply(`*[ ℹ️ ] Responde a un Vídeo con el comando:* _${usedPrefix + command}_`)
+
+      let packName = 'Shadow'
+      let authorName = `by: ${userName}` // Ahora aparece el nombre del usuario
+
+      let stiker = false
+      try {
+        stiker = await sticker(img, false, packName, authorName)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        if (!stiker) {
+          let out = await uploadFile(img)
+          stiker = await sticker(false, out, packName, authorName)
+        }
+      }
+      conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    } else if (/image/g.test(mime)) {
+      let packName = args[0] || 'Shadow'
+      let authorName = args[1] || `by: ${userName}` // Aquí también
+
+      let img = await q.download?.()
+      let stiker = false
+      try {
+        let pack = 'SHADOW'
+        let author = `Used by: ${userName}` // Cambia el nombre en la metadata del sticker
+        stiker = await addExif(img, pack, author)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        if (!stiker) {
+          stiker = await createSticker(img, false, packName, authorName)
+        }
+      }
+      conn.sendMessage(m.chat, { text: `Aquí tienes tu sticker, ${userName}!` })
+    } else {
+      conn.reply(m.chat, '*[ 😏 ] Responde a una imagen o video para convertirlo en sticker.*', m)
+    }
+  } catch (e) {
+    console.error(e)
+    m.reply('Error')
+  }
 }
