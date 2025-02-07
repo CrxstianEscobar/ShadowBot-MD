716f97215bc77c3a26bfcc6c451426cd24ac6109
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-06 19:35:04 -0500

    Actualizar grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn }) => {
+/*let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, `¡Este chat no está registrado!.`, m)
 let chat = global.db.data.chats[m.chat]
 if (!chat.isBanned) return conn.reply(m.chat, `*Shadow* ya estába activado.`, m)
@@ -8,5 +8,21 @@ await conn.reply(m.chat, `Has *activado* a *Shadow*!`, m)
 handler.help = ['unbanchat'];
 handler.tags = ['grupo'];
 handler.command = ['unbanchat', 'vx']
+
+export default handler*/
+
+let handler = async (m, { conn }) => {
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🍭l🍬 *¡Este chat no está registrado!*', m)
+let chat = global.db.data.chats[m.chat]
+if (!chat.isBanned) return conn.reply(m.chat, '🍭 *¡Yuki-Bot no está baneada en este chat!*', m)
+chat.isBanned = false
+await conn.reply(m.chat, '🍬 *¡Yuki-Bot ya fué desbaneada en este chat!*', m)
+}
+handler.help = ['unbanchat'];
+handler.tags = ['grupo'];
+handler.command = ['unbanchat','desbanearchat','desbanchat']
+handler.admin = true 
+handler.botAdmin = true
+handler.group = true
 
 export default handler
