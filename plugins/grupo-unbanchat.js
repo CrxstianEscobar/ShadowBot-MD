5277fc7189ce5fcd504d92dc28e8d6d52078e434
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-06 19:07:27 -0500

    Actualizar grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,43 +1,12 @@
-/*let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🎌 *¡Este chat no está registrado!*', m)
+let handler = async (m, { conn }) => {
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, `¡Este chat no está registrado!.`, m)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '[🌠] *El bot no está baneado en este chat*', m)
+if (!chat.isBanned) return conn.reply(m.chat, `*Shadow* ya estába activado.`, m)
 chat.isBanned = false
-await conn.reply(m.chat, `ya fué desbaneado en este chat*`, m)
+await conn.reply(m.chat, `Has *activado* a *Shadow*!`, m)
 }
 handler.help = ['unbanchat'];
-handler.tags = ['owner'];
-handler.command = ['unbanchat','desbanearchat','desbanchat']
-//handler.mods = true
-handler.botAdmin = false
-handler.group = false
+handler.tags = ['grupo'];
+handler.command = ['unbanchat', 'vx']
 
-export default handler
-
-let handler = async (m) => {
-
-global.db.data.chats[m.chat].isBanned = false
-conn.reply(m.chat, `*[ ℹ️ ] Se activó Shadow en este grupo.*`, m)
-
-}
-handler.help = ['unbanchat']
-handler.tags = ['grupo']
-handler.command = ['unbanchat']
-
-handler.botAdmin = true
-handler.admin = true 
-handler.group = true
-
-export default handler*/
-
-let handler = async (m, { conn, isAdmin, isROwner} ) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '🚩 Bot activo en este grupo.', m, rcanal)
-    await m.react('✅')
-}
-handler.help = ['desbanearbot']
-handler.tags = ['group']
-handler.command = ['xcx', 'unbanchat']
-handler.group = true 
 export default handler
